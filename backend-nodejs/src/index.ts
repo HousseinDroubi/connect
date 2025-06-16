dfe34a9bf2ca0f9615deec21dc4a356f1a16db67
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-16 18:29:50 +0300

    refactor: update importing routes name

--- backend-nodejs/src/index.ts
+++ backend-nodejs/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import router from "./routes/auth.route";
+import authRoutes from "./routes/auth.route";
 import { connectToDB } from "./db/db.connection";
 
 dotenv.config();
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/auth", router);
+app.use("/auth", authRoutes);
 
 connectToDB();
 
