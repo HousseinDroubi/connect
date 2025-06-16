e7b322135692a7f09b8b90bb8c23340ac2e8a450
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-16 17:35:40 +0300

    refactor: implement create user account body interface and update user model interface

--- backend-nodejs/src/interfaces/model.interface.ts
+++ backend-nodejs/src/interfaces/model.interface.ts
@@ -1,10 +1,14 @@
-interface userModelInterface {
+interface createUserAccountBodyInterface {
   email: string;
   username: string;
   pin: number;
   password: string;
+}
+
+interface userModelInterface extends createUserAccountBodyInterface {
   profile_url: string;
-  created_at: Date;
+  is_verified?: boolean;
+  created_at?: Date;
 }
 
 export { userModelInterface };
