12c6b8b079289ffae39bc880324730cb0f547cec
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-17 01:23:14 +0300

    feat: implement token model

---
+++ backend-nodejs/src/models/token.model.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { tokenModelInterface } from "../interfaces/model.interface";
+
+const schema = new mongoose.Schema<tokenModelInterface>({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+  value: {
+    type: String,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    required: false,
+    default: Date.now,
+    index: { expires: "1h" },
+  },
+});
+
+const Token = mongoose.model("tokens", schema);
+export { Token };
