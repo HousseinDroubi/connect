243fc09efdd22b94605d47ea2a4a910653718a9f
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-16 17:23:16 +0300

    feat: implement user model interface

---
+++ backend-nodejs/src/interfaces/model.interface.ts
@@ -0,0 +1,10 @@
+interface userModelInterface {
+  email: string;
+  username: string;
+  pin: number;
+  password: string;
+  profile_url: string;
+  created_at: Date;
+}
+
+export { userModelInterface };
