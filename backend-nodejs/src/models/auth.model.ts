00c57e626571bff2c0e9e69d9bbbcb231a14d041
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-16 17:27:01 +0300

    feat: implement user model

---
+++ backend-nodejs/src/models/auth.model.ts
@@ -0,0 +1,30 @@
+import mongoose, { now } from "mongoose";
+import { userModelInterface } from "../interfaces/model.interface";
+const schema = new mongoose.Schema<userModelInterface>({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profile_url: {
+    type: String,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+    required: false,
+  },
+});
+
+const User = mongoose.model("users", schema);
+export default User;
