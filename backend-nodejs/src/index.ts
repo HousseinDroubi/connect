d3965b9da744674033047830ec2aa4681433a882
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-16 12:09:33 +0300

    feat: add general path for auth routes

--- backend-nodejs/src/index.ts
+++ backend-nodejs/src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import router from "./routes/auth.route";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
+
+app.use("/auth", router);
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
