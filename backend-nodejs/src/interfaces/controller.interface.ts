923794aa1b1c690604370a99a78e1b06f91ffbf9
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-16 23:21:13 +0300

    refactor: separate create user account body interface

---
+++ backend-nodejs/src/interfaces/controller.interface.ts
@@ -0,0 +1,9 @@
+interface createUserAccountBodyInterface {
+  email: string;
+  username: string;
+  pin: number;
+  password: string;
+  file_name: string;
+}
+
+export { createUserAccountBodyInterface };
--- backend-nodejs/src/interfaces/model.interface.ts
+++ backend-nodejs/src/interfaces/model.interface.ts
@@ -1,14 +1,8 @@
-interface createUserAccountBodyInterface {
-  email: string;
-  username: string;
-  pin: number;
-  password: string;
-  file_name: string;
-}
+import { createUserAccountBodyInterface } from "./controller.interface";
 
 interface userModelInterface extends createUserAccountBodyInterface {
   is_verified?: boolean;
   created_at?: Date;
 }
 
-export { createUserAccountBodyInterface, userModelInterface };
+export { userModelInterface };
