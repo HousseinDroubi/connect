92927afcfc70beb92279bfa54facef6c24d77f9b
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-17 00:45:33 +0300

    fix: override profile_url instead of file_name

--- backend-nodejs/src/interfaces/model.interface.ts
+++ backend-nodejs/src/interfaces/model.interface.ts
@@ -1,6 +1,8 @@
 import { createUserAccountBodyInterface } from "./controller.interface";
 
-interface userModelInterface extends createUserAccountBodyInterface {
+interface userModelInterface
+  extends Omit<createUserAccountBodyInterface, "file_name"> {
+  profile_url: string;
   is_verified?: boolean;
   created_at?: Date;
 }
