78f9dcecc3e4aab94e946a3d9e46d11c98c672fe
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-18 00:50:10 +0300

    refactor: update validations

--- backend-nodejs/src/validations/auth.validation.ts
+++ backend-nodejs/src/validations/auth.validation.ts
@@ -9,6 +9,7 @@ const validateCreateAccount = (data: createUserAccountBodyInterface) => {
   const schema = joi.object({
     email: joi.string().required().label("Email").email().messages({
       "any.required": "email_is_required",
+      "string.base": "email_must_be_of_type_string",
       "string.email": "invalid_email",
       "string.empty": "email_is_not_allowed_to_be_empty",
     }),
@@ -18,6 +19,7 @@ const validateCreateAccount = (data: createUserAccountBodyInterface) => {
       .label("Pin")
       .pattern(/^\d{6}$/)
       .messages({
+        "string.base": "pin_must_be_of_type_string",
         "string.pattern.base": "pin_must_be_exactly_6_digits",
         "any.required": "pin_is_required",
         "string.empty": "pin_is_not_allowed_to_be_empty",
@@ -29,6 +31,7 @@ const validateCreateAccount = (data: createUserAccountBodyInterface) => {
       .min(5)
       .max(20)
       .messages({
+        "string.base": "password_must_be_of_type_string",
         "any.required": "password_is_required",
         "string.empty": "password_is_not_allowed_to_be_empty",
         "string.min": "password_must_be_minimum_5_digits",
@@ -41,6 +44,7 @@ const validateCreateAccount = (data: createUserAccountBodyInterface) => {
       .min(3)
       .max(10)
       .messages({
+        "string.base": "username_must_be_of_type_string",
         "any.required": "username_is_required",
         "string.empty": "username_is_not_allowed_to_be_empty",
         "string.min": "username_must_be_minimum_3_digits",
@@ -57,6 +61,7 @@ const validateActivateAccount = (data: verifyAccountParamsInterface) => {
   const schema = joi.object({
     token: joi.string().required().label("Token").messages({
       "any.required": "token_is_required",
+      "string.base": "token_must_be_of_type_string",
       "string.empty": "token_is_not_allowed_to_be_empty",
     }),
   });
