287ee7bd254e756ff8d0dcad21586ef4c635b0b6
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-19 01:01:13 +0300

    chore: remove logs

--- backend-nodejs/src/middlewares/auth.middleware.ts
+++ backend-nodejs/src/middlewares/auth.middleware.ts
@@ -7,8 +7,7 @@ const isUserAuthenticated = (
   next: NextFunction
 ) => {
   const _id = getIdFromToken(request.headers.authorization);
-  console.log(request.headers.authorization);
-  console.log(isObjectIdValid(_id!));
+
   if (!_id) return response.status(401).json({ error: "invalid_id" });
   request.body.user_id = _id;
   next();
