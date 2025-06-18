eabe0ca00a0b8512cb45452cb544042f2b56ec6a
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-19 00:57:58 +0300

    fix: add user id to body rather than request

--- backend-nodejs/src/middlewares/auth.middleware.ts
+++ backend-nodejs/src/middlewares/auth.middleware.ts
@@ -1,11 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { getIdFromToken } from "../functions/general";
-
-declare module "express-serve-static-core" {
-  interface Request {
-    user_id?: string;
-  }
-}
+import { getIdFromToken, isObjectIdValid } from "../functions/general";
 
 const isUserAuthenticated = (
   request: Request,
@@ -13,8 +7,10 @@ const isUserAuthenticated = (
   next: NextFunction
 ) => {
   const _id = getIdFromToken(request.headers.authorization);
+  console.log(request.headers.authorization);
+  console.log(isObjectIdValid(_id!));
   if (!_id) return response.status(401).json({ error: "invalid_id" });
-  request.user_id = _id;
+  request.body.user_id = _id;
   next();
 };
 
