3754415a57eaa42208698830a94d55011cd66741
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-19 18:43:15 +0300

    feat: implement login response interface

--- backend-nodejs/src/interfaces/responses/auth.controller.responses.interfaces.ts
+++ backend-nodejs/src/interfaces/responses/auth.controller.responses.interfaces.ts
@@ -1,6 +1,34 @@
+import mongoose from "mongoose";
+
 interface updateProfileResponseInterface {
   result: string;
   new_profile_url?: string;
+}
+
+interface loginResponseInterface {
+  _id: mongoose.Types.ObjectId;
+  email: string;
+  username: string;
+  pin: number;
+  profile_url: string;
+  token: string;
+  conversations: [
+    {
+      recipient: {
+        profile_url: string;
+        username: string;
+      };
+      created_at: Date;
+      last_message: {
+        sender: mongoose.Types.ObjectId;
+        receiver: mongoose.Types.ObjectId;
+        created_at: Date;
+        deleted: boolean;
+        is_text: boolean;
+        content: string;
+      };
+    }
+  ];
 }
 
 export { updateProfileResponseInterface };
