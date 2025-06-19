c10c5012c3dc259b5b347ae96bb186bac6c3df51
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-20 01:04:53 +0300

    chore: remove not needed module

--- backend-nodejs/src/interfaces/models/model.interface.ts
+++ backend-nodejs/src/interfaces/models/model.interface.ts
@@ -1,4 +1,4 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 import { createUserAccountBodyInterface } from "../controllers/auth.controller.interfaces";
 
 interface userModelInterface
