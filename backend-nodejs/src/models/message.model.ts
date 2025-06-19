8e33e7bb70125e33f56b84758219178ff51f8653
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-19 15:58:07 +0300

    feat: implement message model

---
+++ backend-nodejs/src/models/message.model.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import { messageModelInterface } from "../interfaces/models/model.interface";
+
+const schema = new mongoose.Schema<messageModelInterface>({
+  sender: mongoose.Schema.Types.ObjectId,
+  receiver: mongoose.Schema.Types.ObjectId,
+  created_at: {
+    type: Date,
+    required: false,
+    default: Date.now,
+  },
+  deleted_from_sender_at: {
+    type: Date,
+    required: false,
+    default: null,
+  },
+  deleted_from_others_at: {
+    type: Date,
+    required: false,
+    default: null,
+  },
+  is_text: {
+    type: Boolean,
+    required: false,
+    default: true,
+  },
+  content: {
+    type: String,
+    required: false,
+  },
+  profile_url: {
+    type: String,
+    required: false,
+    default: null,
+  },
+});
+
+const Message = mongoose.model("messages", schema);
+export { Message };
