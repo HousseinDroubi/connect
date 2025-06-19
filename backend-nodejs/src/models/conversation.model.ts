cec5b8ed0c39041cd42d347230a0761263d62313
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-19 15:54:11 +0300

    feat: implement conversation model

---
+++ backend-nodejs/src/models/conversation.model.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { conversationModelInterface } from "../interfaces/models/model.interface";
+
+const schema = new mongoose.Schema<conversationModelInterface>({
+  between: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+    },
+  ],
+  created_at: {
+    type: Date,
+    required: false,
+    default: Date.now,
+  },
+  last_message: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
+  deleted_from: {
+    type: [mongoose.Schema.Types.ObjectId],
+  },
+});
+
+const Conversation = mongoose.model("conversations", schema);
+export { Conversation };
