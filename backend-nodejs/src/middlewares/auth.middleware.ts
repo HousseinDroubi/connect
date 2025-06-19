6340a15ef9caceb6cbcfa621ff737f2dbec83fe5
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-19 11:21:36 +0300

    feat: implement user account deleted middleware

--- backend-nodejs/src/middlewares/auth.middleware.ts
+++ backend-nodejs/src/middlewares/auth.middleware.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { getIdFromToken, isObjectIdValid } from "../functions/general";
+import { request } from "http";
+import User from "../models/user.model";
 
 const isUserAuthenticated = (
   request: Request,
@@ -13,4 +15,25 @@ const isUserAuthenticated = (
   next();
 };
 
-export { isUserAuthenticated };
+const isUserAccountDeleted = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const { _id } = request.body;
+  const user = await User.findById(_id);
+
+  if (!user)
+    return response.status(404).json({
+      result: "user_not_found",
+    });
+
+  if (user.deleted_at)
+    return response.status(410).json({
+      result: "user_account_deleted",
+    });
+
+  next();
+};
+
+export { isUserAuthenticated, isUserAccountDeleted };
