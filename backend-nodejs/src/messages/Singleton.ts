c50826a29f9c5d1ad81cafd921bd50c6907109cf
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-20 22:44:37 +0300

    feat: initialize launch ws function

--- backend-nodejs/src/messages/Singleton.ts
+++ backend-nodejs/src/messages/Singleton.ts
@@ -1,6 +1,14 @@
 class Singleton {
   public static instance: Singleton;
 
+  private constructor() {
+    this.launchWebSocket();
+  }
+
+  private launchWebSocket(): void {
+    console.log("Launching ws");
+  }
+
   public getInstance(): Singleton {
     if (!Singleton.instance) {
       Singleton.instance = new Singleton();
