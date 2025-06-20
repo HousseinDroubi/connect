701f99272bfb23de85e17fbd2c9ab7c411f540a3
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-20 22:43:32 +0300

    feat: implement sinleton instance

---
+++ backend-nodejs/src/messages/Singleton.ts
@@ -0,0 +1,10 @@
+class Singleton {
+  public static instance: Singleton;
+
+  public getInstance(): Singleton {
+    if (!Singleton.instance) {
+      Singleton.instance = new Singleton();
+    }
+    return Singleton.instance;
+  }
+}
