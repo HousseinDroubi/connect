26c0a1dd157cc167cd3c1e3bb5e40891c98331da
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-21 17:55:03 +0300

    feat: implement even interface

--- backend-nodejs/src/interfaces/functions/functions.interface.ts
+++ backend-nodejs/src/interfaces/functions/functions.interface.ts
@@ -17,8 +17,25 @@ interface toggleUserStatusToOthersToFrontendInterface
   is_online: boolean;
 }
 
+interface eventInterface {
+  event_name:
+    | "toggle_user_status"
+    | "new_message"
+    | "edit_message"
+    | "delete_message_for_others";
+  from: string;
+  is_online?: boolean;
+  message?: {
+    _id: string;
+    is_text: boolean;
+    to: string;
+    content: string;
+  };
+}
+
 export {
   moveFileInterface,
   saveWebSocketIntoWebSocketsMapInterface,
   toggleUserStatusToOthersToFrontendInterface,
+  eventInterface,
 };
