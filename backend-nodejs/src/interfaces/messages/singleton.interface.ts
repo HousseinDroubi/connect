38fbfe6adacf96bbab46438154ee1e73e41f5c13
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-21 18:21:34 +0300

    refactor: move event interface to another file

---
+++ backend-nodejs/src/interfaces/messages/singleton.interface.ts
@@ -0,0 +1,17 @@
+interface eventInterface {
+  event_name:
+    | "toggle_user_status"
+    | "new_message"
+    | "edit_message"
+    | "delete_message_for_others";
+  from: string;
+  is_online?: boolean;
+  message?: {
+    _id: string;
+    is_text: boolean;
+    to: string;
+    content: string;
+  };
+}
+
+export { eventInterface };
