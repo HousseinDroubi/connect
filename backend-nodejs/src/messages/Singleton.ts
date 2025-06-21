cab3a62b870c368d1c9806a9486b4419194ce1b2
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-21 21:42:36 +0300

    chore: add comments

--- backend-nodejs/src/messages/Singleton.ts
+++ backend-nodejs/src/messages/Singleton.ts
@@ -25,34 +25,47 @@ class Singleton {
   }
 
   private launchWebSocket(): void {
+    // Delete Ws server instance
     const wss = new WebSocket.Server({ port: Number(process.env.WS_PORT) });
+
+    // Listen for connection open in ws
     wss.on("connection", async (websocket: WebSocket, request: Request) => {
+      // Get user from url by getting token
       const user: userDocumentInterface["user"] | null =
         await getUserFromWebsocketUrl(request.url);
       if (!user) return;
 
+      // Get user id in map
       saveWebSocketIntoWebSocketsMap({
         user_id: String(user._id),
         websocket: websocket,
         websockets_map: Singleton.websockets_map,
       });
 
+      // Toggle user status to online in DB
       await toggleUserStatusIntoDB(user, true);
+
+      // Toggle user status to online for others in frontend
       toggleUserStatusToOthersToFrontend({
         user_id: String(user._id),
         is_online: true,
         websockets_map: Singleton.websockets_map,
       });
 
+      // Listen for message in ws
       websocket.on("message", async (data) => {
+        // Parse message to json
         const new_message: newMessageInterface = JSON.parse(data.toString());
-        const error = validateNewMessage(new_message).error?.details[0].message;
 
+        // Validate message
+        const error = validateNewMessage(new_message).error?.details[0].message;
         if (error) return;
 
+        // Validate user id and their existence in DB
         if (!isObjectIdValid(new_message.to)) return;
         if (!(await User.exists({ _id: new_message.to }))) return;
 
+        // Validate image existence if into DB
         if (!new_message.is_text) {
           if (
             !checkFileExistence(
@@ -62,6 +75,7 @@ class Singleton {
             return;
         }
 
+        // Create new conversation if not existed
         let conversation = await Conversation.findOne({
           between: {
             $all: [new mongoose.Types.ObjectId(new_message.to), user._id],
@@ -75,6 +89,7 @@ class Singleton {
           });
         }
 
+        // Create new message
         const message = await Message.create({
           sender: user._id,
           receiver: new mongoose.Types.ObjectId(new_message.to),
@@ -83,17 +98,24 @@ class Singleton {
           conversation_id: conversation._id,
         });
 
+        // Update conversation last message id
         conversation.last_message = message._id;
         conversation.save();
       });
 
+      // Listen for connection close in ws
       websocket.on("close", async () => {
+        // Toggle user status to offline in DB
         await toggleUserStatusIntoDB(user, false);
+
+        // Toggle user status to offline for others in frontend
         toggleUserStatusToOthersToFrontend({
           user_id: String(user._id),
           is_online: false,
           websockets_map: Singleton.websockets_map,
         });
+
+        // Delete user id from map
         Singleton.websockets_map.delete(String(user._id));
       });
     });
