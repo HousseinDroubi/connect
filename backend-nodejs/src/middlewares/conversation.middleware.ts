31950de1a35588572bc3b9da43bee23ba0678c42
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-22 17:01:16 +0300

    feat: implement checking if conversation id is valid

---
+++ backend-nodejs/src/middlewares/conversation.middleware.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+import { Conversation } from "../models/conversation.model";
+import { isObjectIdValid } from "../functions/general";
+
+const iseConversationExisted = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const { conversation_id } = request.params;
+
+  if (!conversation_id || !isObjectIdValid(conversation_id))
+    return response.status(400).json({
+      result: "conversation_id_is_invalid",
+    });
+
+  const conversation = await Conversation.findById(conversation_id);
+  if (!conversation)
+    return response.status(404).json({
+      result: "conversation_not_found",
+    });
+
+  next();
+};
+
+export { iseConversationExisted };
