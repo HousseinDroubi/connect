5a5e5ed80c911a8fe30b6ce0d4e91fc71dc86929
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-22 21:41:39 +0300

    feat: implement check conversation existence middleware

--- backend-nodejs/src/middlewares/conversation.middleware.ts
+++ backend-nodejs/src/middlewares/conversation.middleware.ts
@@ -1,6 +1,37 @@
 import { Request, Response, NextFunction } from "express";
 import { conversationDocumentInterface } from "../interfaces/documents/conversation.document.interface";
 import { userDocumentInterface } from "../interfaces/documents/user.document.interface";
+import { Conversation } from "../models/conversation.model";
+import User from "../models/user.model";
+
+const isConversationExisted = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const { pin } = request.params;
+  const body: userDocumentInterface = request.body;
+
+  const other_user = await User.findOne({ pin });
+  if (!other_user)
+    return response.status(404).json({
+      result: "user_not_found",
+    });
+
+  const conversation = await Conversation.create({
+    between: [other_user._id, body.user!._id],
+    last_message: null,
+  });
+
+  if (!conversation)
+    return response.status(404).json({
+      result: "conversation_not_found",
+    });
+
+  request.body.conversation = conversation;
+
+  next();
+};
 
 const isUserAuthorizedToAccessConversation = (
   request: Request,
@@ -25,4 +56,4 @@ const isUserAuthorizedToAccessConversation = (
   next();
 };
 
-export { isUserAuthorizedToAccessConversation };
+export { isUserAuthorizedToAccessConversation, isConversationExisted };
