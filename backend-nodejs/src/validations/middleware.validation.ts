a5256438ac2b0603dd90c8b1e554af44a1887d24
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-22 20:00:21 +0300

    refactor: update get conversation messages validation

--- backend-nodejs/src/validations/middleware.validation.ts
+++ backend-nodejs/src/validations/middleware.validation.ts
@@ -2,15 +2,25 @@ import joi from "joi";
 
 const getConversationMessagesValidation = (data: string) => {
   const schema = joi
-    .string()
+    .alternatives()
+    .try(
+      joi
+        .string()
+        .pattern(/^\d{6}$/)
+        .messages({
+          "string.pattern.base": "pin_must_be_exactly_6_digits",
+        }),
+      joi.string().valid("broadcast").messages({
+        "any.only": "pin_must_be_exactly_6_digits_or_broadcast",
+      })
+    )
     .required()
-    .pattern(/^\d{6}$/)
     .label("Pin")
     .messages({
       "string.base": "pin_must_be_of_type_string",
-      "string.pattern.base": "pin_must_be_exactly_6_digits",
       "string.empty": "pin_is_not_allowed_to_be_empty",
     });
+
   return schema.validate(data);
 };
 
