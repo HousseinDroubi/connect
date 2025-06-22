e21df65eed369252e45611df21259bd49a041bd2
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-22 15:23:30 +0300

    refactor: check message id existence and its validity

--- backend-nodejs/src/middlewares/message.middleware.ts
+++ backend-nodejs/src/middlewares/message.middleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { Message } from "../models/message.model";
+import { isObjectIdValid } from "../functions/general";
 
 // This middleware takes message_id from token and find whethere it exists in DB or not
 const isMessageExisted = async (
@@ -8,6 +9,11 @@ const isMessageExisted = async (
   next: NextFunction
 ) => {
   const { message_id } = request.params;
+  if (!message_id || !isObjectIdValid(message_id))
+    return respone.status(400).json({
+      result: "message_id_required",
+    });
+
   const message = await Message.findById(message_id);
   if (!message) {
     return respone.status(404).json({
