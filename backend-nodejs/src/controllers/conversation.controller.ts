1de46b8d306ac26873b9f7d85b28d65d90c06278
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-22 16:26:30 +0300

    feat: initialize conversation functions

---
+++ backend-nodejs/src/controllers/conversation.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+
+const deleteConversation = (request: Request, response: Response) => {
+  // deleteConversation
+};
+
+const restoreConversation = (request: Request, response: Response) => {
+  // getConversationMessages
+};
+
+const getConversationMessages = (request: Request, response: Response) => {
+  // getConversationMessages
+};
+
+export { deleteConversation, restoreConversation, getConversationMessages };
