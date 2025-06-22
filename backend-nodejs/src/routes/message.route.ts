86da11cb011099505f9d22b22de7beff8a831a3c
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-22 11:18:36 +0300

    feat: implement upload image route middlewares

--- backend-nodejs/src/routes/message.route.ts
+++ backend-nodejs/src/routes/message.route.ts
@@ -1,9 +1,20 @@
 import { Router } from "express";
 import { uploadImage } from "../controllers/message.controller";
+import {
+  isUserAccountDeleted,
+  isUserAuthenticated,
+} from "../middlewares/auth.middleware";
+import { upload } from "../middlewares/multer.middleware";
 
 const router = Router();
 
-router.post("/upload_image", uploadImage);
+router.post(
+  "/upload_image",
+  isUserAuthenticated,
+  isUserAccountDeleted,
+  upload.single("image"),
+  uploadImage
+);
 router.get("/view_image/:message_id", uploadImage);
 
 export default router;
