5c8b0cbcc9996e450ac36baa0c9a452a092b36bd
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-22 21:39:26 +0300

    feat: return appropriate conversation

--- backend-nodejs/src/controllers/conversation.controller.ts
+++ backend-nodejs/src/controllers/conversation.controller.ts
@@ -1,16 +1,44 @@
 import { Request, Response } from "express";
 import { userDocumentInterface } from "../interfaces/documents/user.document.interface";
-import { conversationDocumentInterface } from "../interfaces/documents/conversation.document.interface";
 import { Message } from "../models/message.model";
+import { Conversation } from "../models/conversation.model";
+import User from "../models/user.model";
 
 const getConversationMessages = async (
   request: Request,
   response: Response
 ) => {
-  const body: userDocumentInterface & conversationDocumentInterface =
-    request.body;
-  if (!body.user || !body.conversation)
-    throw new Error("Neither user nor conversation in body");
+  const { pin } = request.params;
+  const body: userDocumentInterface = request.body;
+
+  if (!body.user) throw new Error("User is not in body");
+
+  let conversation;
+
+  if (pin === "broadcast") {
+    conversation = await Conversation.findOne({ between: null });
+  } else {
+    const other_user = await User.findOne({ pin });
+    if (!other_user)
+      return response.status(404).json({
+        result: "user_not_found",
+      });
+    conversation = await Conversation.findOne({
+      between: { $all: [body.user._id, other_user._id] },
+    });
+    if (!conversation) {
+      conversation = await Conversation.create({
+        between: [other_user._id, body.user!._id],
+        last_message: null,
+      });
+      return response.status(201).json({
+        result: "conversation_added",
+        conversation_id: conversation._id,
+      });
+    }
+  }
+
+  if (!conversation) throw new Error("Conversation is not existed");
 
   const messages = await Message.find({
     $or: [
@@ -18,7 +46,7 @@ const getConversationMessages = async (
       { receiver: body.user._id },
       { receiver: null },
     ],
-    conversation_id: body.conversation._id,
+    conversation_id: conversation._id,
     deleted_for_others_at: null,
     deleted_for: { $nin: [body.user._id] },
   });
