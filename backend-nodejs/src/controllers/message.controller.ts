da81fafc6c99a41cd6e04850b5ebc714e4e6a4b1
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-22 11:14:03 +0300

    feat: initialize message controller functions

---
+++ backend-nodejs/src/controllers/message.controller.ts
@@ -0,0 +1,15 @@
+import {Request,Response} from 'express';
+
+const uploadImage = (request:Request,response:Response)=>{
+    // uploadImage
+}
+
+
+const viewImage = (request:Request,response:Response)=>{
+    // viewImage
+}
+
+export {
+    uploadImage,
+    viewImage
+}
