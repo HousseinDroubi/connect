1ef4138a384b9affccd6d0f4a3aac7e6499eba94
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-25 21:32:26 +0300

    feart: implement view other profile param validation

---
+++ backend-nodejs/src/validations/user.validation.ts
@@ -0,0 +1,10 @@
+import joi from "joi";
+
+const validateViewOtherUserProfile = (data: string) => {
+  const schema = joi.string().required();
+  return schema.validate(data);
+};
+
+export {
+  validateViewOtherUserProfile
+};
