02524431781a2bbf8696b2967087b59efe181619
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-26 01:27:24 +0300

    refactor: stop request if user is visiting their profiles

--- backend-nodejs/src/controllers/user.controller.ts
+++ backend-nodejs/src/controllers/user.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import User from "../models/user.model";
+import { userDocumentInterface } from "../interfaces/documents/user.document.interface";
 
 const viewOtherUserProfile = async (request: Request, response: Response) => {
+  const body: userDocumentInterface = request.body;
   const other_user_id: string = request.params.user_id;
 
   const other_user = await User.findById(other_user_id);
@@ -10,6 +12,12 @@ const viewOtherUserProfile = async (request: Request, response: Response) => {
     return response.status(400).json({
       result: "user_not_found",
     });
+
+  if (String(other_user._id) == String(body.user!._id)) {
+    return response.status(400).json({
+      result: "same_account",
+    });
+  }
 
   return response.status(200).json({
     username: other_user.username,
