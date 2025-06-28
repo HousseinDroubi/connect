c485340cb231049f4bc1f6fcd4931ce8b4eb3824
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-28 09:56:43 +0300

    feat: add funtion and text for button

--- frontend-web-react/src/components/Button.tsx
+++ frontend-web-react/src/components/Button.tsx
@@ -1,5 +1,7 @@
-const Button = () => {
-  return <button>Click here</button>;
+import { buttonInterface } from "../interfaces/components/components.interfaces";
+
+const Button: React.FC<buttonInterface> = ({ fn, button_text }) => {
+  return <button onClick={fn}>{button_text}</button>;
 };
 
 export default Button;
