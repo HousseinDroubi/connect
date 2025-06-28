2f92cb93fe4bdb96b8d23909fb472f8b1371dcef
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-28 12:21:58 +0300

    feat: implement forgot password style

--- frontend-web-react/src/pages/ForgotPassword.tsx
+++ frontend-web-react/src/pages/ForgotPassword.tsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import Button from "../components/Button";
+import TextField from "../components/TextField";
+import TitleBig from "../components/TitleBig";
+
 const ForgotPassword = () => {
-  return <div>Hello from ForgotPassword</div>;
+  const [emailText, setEmailText] = useState<string>("");
+
+  const forgotPassowrd = () => {
+    console.log(`email is ${emailText}`);
+  };
+
+  return (
+    <div className="h-screen w-full flex flex-col items-center justify-center">
+      <TitleBig title="Forgot Password" />
+      <section className="flex flex-col items-center justify-center w-fit h-48 ">
+        <TextField
+          title="Email"
+          hint="Enter your email"
+          setText={setEmailText}
+        />
+        <div className="mt-5">
+          <Button button_text="Send email" fn={forgotPassowrd} />
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default ForgotPassword;
