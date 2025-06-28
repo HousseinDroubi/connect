a0f3d26cf981e611810b0249bb9dd5beeb3ee4fa
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-28 10:11:09 +0300

    feat: implemnet button style

--- frontend-web-react/src/components/Button.tsx
+++ frontend-web-react/src/components/Button.tsx
@@ -1,7 +1,16 @@
 import { buttonInterface } from "../interfaces/components/components.interfaces";
 
-const Button: React.FC<buttonInterface> = ({ fn, button_text }) => {
-  return <button onClick={fn}>{button_text}</button>;
+const Button: React.FC<buttonInterface> = ({ fn, button_text, is_small }) => {
+  return (
+    <button
+      onClick={fn}
+      className={`bg-blue ${
+        is_small ? "w-28" : "w-44"
+      } h-10 rounded-md text-white cursor-pointer font-medium`}
+    >
+      {button_text}
+    </button>
+  );
 };
 
 export default Button;
--- frontend-web-react/tailwind.config.js
+++ frontend-web-react/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#0C7FF2",
+      },
+    },
   },
   plugins: [],
 };
