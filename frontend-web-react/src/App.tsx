7d1aaa66b8214ecaf76a172470a4e00240f59c3a
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-28 09:54:59 +0300

    feat: implement routes for authentication pages

--- frontend-web-react/src/App.tsx
+++ frontend-web-react/src/App.tsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import CreateAccount from "./pages/CreateAccount";
+import VerifyAccount from "./pages/VerifyAccount";
+import ForgotPassword from "./pages/ForgotPassword";
+import UpdateForgottenPassword from "./pages/UpdateForgottenPassword";
 function App() {
-  return <div>Hello there</div>;
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/create_new_account" element={<CreateAccount />} />
+      <Route path="/verify_account/:token" element={<VerifyAccount />} />
+      <Route path="/forgot_password" element={<ForgotPassword />} />
+      <Route
+        path="/update_forgotten_password"
+        element={<UpdateForgottenPassword />}
+      />
+    </Routes>
+  </BrowserRouter>;
 }
 
 export default App;
