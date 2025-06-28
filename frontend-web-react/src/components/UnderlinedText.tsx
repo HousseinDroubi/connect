d02fc4390c84141b99d05944d11db573ff77d51b
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-28 12:07:13 +0300

    feat: implement component style

--- frontend-web-react/src/components/UnderlinedText.tsx
+++ frontend-web-react/src/components/UnderlinedText.tsx
@@ -5,7 +5,11 @@ const UnderlinedText: React.FC<UnderlinedTextComponentInterface> = ({
   text,
   href,
 }) => {
-  return <a href={href}>{text}</a>;
+  return (
+    <a href={href} className="text-dusty_blue font-semibold text-lg underline">
+      {text}
+    </a>
+  );
 };
 
 export default UnderlinedText;
