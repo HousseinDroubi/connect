7e1cbffc7c5e8fa42353b5c419c62990519c78d0
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-28 14:17:50 +0300

    feat: implement profile component

--- frontend-web-react/src/components/Profile.tsx
+++ frontend-web-react/src/components/Profile.tsx
@@ -1,7 +1,20 @@
 import { profileComponentInterface } from "../interfaces/components/components.interfaces";
-
+import DefaultImageIcon from "../assets/profile_default.png";
+import AddImageIcon from "../assets/add_image.png";
 const Profile: React.FC<profileComponentInterface> = ({ image, setImage }) => {
-  return <></>;
+  return (
+    <div className="size-28 bg-blacsk">
+      <label className="relative cursor-pointer" htmlFor="input_image">
+        <section className="size-28 rounded-full border-[0.5px] border-blue flex items-center justify-center">
+          {!image && <img src={DefaultImageIcon} />}
+        </section>
+        <section className="absolute right-0 bottom-2">
+          <img src={AddImageIcon} height={23} width={23} />
+        </section>
+        <input className="hidden" type="file" id="input_image" />
+      </label>
+    </div>
+  );
 };
 
 export default Profile;
