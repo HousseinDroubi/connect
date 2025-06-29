d11f0ef3613cb0667a939e512abfabbef4d8553e
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-29 09:38:09 +0300

    feat: implement verify icon style

--- frontend-web-react/src/pages/VerifyAccount.tsx
+++ frontend-web-react/src/pages/VerifyAccount.tsx
@@ -1,5 +1,24 @@
+import TitleBig from "../components/TitleBig";
+import { useParams } from "react-router-dom";
+import DoneIcon from "../assets/done.png";
+import ErrorIcon from "../assets/error.png";
+import { useEffect } from "react";
+
 const VerifyAccount = () => {
-  return <div>Hello from VerifyAccount</div>;
+  const { token } = useParams();
+  useEffect(() => {
+    console.log(token);
+  }, []);
+
+  return (
+    <div className="h-screen w-full flex flex-col items-center justify-center">
+      <TitleBig title="Verify your account" />
+      <section className="flex flex-col items-center justify-center w-fit h-72">
+        <img src={DoneIcon} height={200} width={200} />
+        <p>Your account has been veified. You can login now</p>
+      </section>
+    </div>
+  );
 };
 
 export default VerifyAccount;
