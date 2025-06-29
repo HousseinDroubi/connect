5f92ab19255ba4e2ccc3d50ab1d03e7afb05b901
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-29 11:36:56 +0300

    feat: implement popup component interface

--- frontend-web-react/src/interfaces/components/components.interfaces.ts
+++ frontend-web-react/src/interfaces/components/components.interfaces.ts
@@ -32,6 +32,47 @@ interface profileComponentInterface {
   setImage: React.Dispatch<React.SetStateAction<File | null>>;
 }
 
+interface popupBasicComponentInterface {
+  seen: boolean;
+  setSeen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+interface popupLoadingComponentInterface extends popupBasicComponentInterface {
+  for: "loading";
+}
+
+interface popupConfirmationComponentInterface
+  extends popupBasicComponentInterface {
+  for: "confirmation";
+  confimationFunction: () => void;
+}
+
+interface popupDeleteMessageForOtherUserComponentInterface
+  extends popupBasicComponentInterface {
+  for: "delete_message_for_other_user";
+  deleteForMe: () => void;
+}
+
+interface popupDeleteMessageForUserComponentInterface
+  extends popupBasicComponentInterface {
+  for: "delete_message_for_user";
+  deleteForMe: () => void;
+  deleteForEveryOne: () => void;
+}
+
+interface popupEditMessageForUserComponentInterface
+  extends popupBasicComponentInterface {
+  for: "edit_message_for_user";
+  setText: React.Dispatch<React.SetStateAction<string>>;
+}
+
+type popupComponentInterface =
+  | popupLoadingComponentInterface
+  | popupConfirmationComponentInterface
+  | popupDeleteMessageForOtherUserComponentInterface
+  | popupDeleteMessageForUserComponentInterface
+  | popupEditMessageForUserComponentInterface;
+
 export type {
   buttonComponentInterface,
   titleBigComponentInterface,
@@ -39,4 +80,5 @@ export type {
   textFieldComponentInterface,
   underlinedTextComponentInterface,
   profileComponentInterface,
+  popupComponentInterface,
 };
