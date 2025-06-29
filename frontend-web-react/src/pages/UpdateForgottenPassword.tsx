3afe06b3e8bb35b35796f5cda91c2fcbd47b0892
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-29 09:04:45 +0300

    feat: implement update forgotten password page

--- frontend-web-react/src/pages/UpdateForgottenPassword.tsx
+++ frontend-web-react/src/pages/UpdateForgottenPassword.tsx
@@ -1,5 +1,44 @@
+import { useState } from "react";
+import Button from "../components/Button";
+import Logo from "../components/Logo";
+import TextField from "../components/TextField";
+import TitleBig from "../components/TitleBig";
+import UnderlinedText from "../components/UnderlinedText";
+
 const UpdateForgottenPassword = () => {
-  return <div>Hello from UpdateForgottenPassword</div>;
+  const [password, setPasswordText] = useState<string>("");
+  const [confrimationPasswordText, setConfrimationPasswordText] =
+    useState<string>("");
+
+  const changePassword = () => {
+    console.log(`password is ${password}`);
+    console.log(`confirmation password is ${confrimationPasswordText}`);
+  };
+
+  return (
+    <div className="h-screen w-full flex flex-col items-center justify-center">
+      <TitleBig title="Update Password" />
+      <section className="flex flex-col items-center justify-center w-fit h-100">
+        <TextField
+          title="New Password"
+          hint="Enter your email or pin"
+          setText={setPasswordText}
+          is_password
+        />
+        <div className="mt-5">
+          <TextField
+            title="Confirmation Password"
+            hint="Re-enter your password"
+            setText={setConfrimationPasswordText}
+            is_password
+          />
+        </div>
+        <div className="mt-5">
+          <Button button_text="Change Password" fn={changePassword} />
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default UpdateForgottenPassword;
