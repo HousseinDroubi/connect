6561193c524823191bd29b09dcbd0c7272d7bab0
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-29 11:56:00 +0300

    fix: return empty tags if seen is falsy value

--- frontend-web-react/src/components/Popup.tsx
+++ frontend-web-react/src/components/Popup.tsx
@@ -1,5 +1,8 @@
-const Popup = () => {
-  return <></>;
+import { popupComponentInterface } from "../interfaces/components/components.interfaces";
+
+const Popup: React.FC<popupComponentInterface> = (props) => {
+  if (!props.seen) return <></>;
+  return props.seen && <div></div>;
 };
 
 export default Popup;
