3f50e581892bc812a86f441d4a29f59ecca1d5aa
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-29 10:14:48 +0300

    refactor: update button style

--- frontend-web-react/src/components/Button.tsx
+++ frontend-web-react/src/components/Button.tsx
@@ -4,13 +4,17 @@ const Button: React.FC<buttonComponentInterface> = ({
   fn,
   button_text,
   is_small,
+  is_disabled,
+  is_colored_red,
 }) => {
   return (
     <button
       onClick={fn}
-      className={`bg-blue ${
+      className={`${is_colored_red ? "bg-red-600" : "bg-blue"} ${
+        is_disabled && "bg-gray-300 cursor-not-allowed"
+      } ${
         is_small ? "w-28" : "w-44"
-      } h-10 rounded-md text-white cursor-pointer font-medium`}
+      } h-10 rounded-md cursor-pointer text-white font-medium`}
     >
       {button_text}
     </button>
--- frontend-web-react/src/interfaces/components/components.interfaces.ts
+++ frontend-web-react/src/interfaces/components/components.interfaces.ts
@@ -1,7 +1,9 @@
 interface buttonComponentInterface {
-  is_small?: boolean;
   button_text: string;
   fn: () => void;
+  is_small?: boolean;
+  is_disabled?: boolean;
+  is_colored_red?: boolean;
 }
 
 interface titleBigComponentInterface {
