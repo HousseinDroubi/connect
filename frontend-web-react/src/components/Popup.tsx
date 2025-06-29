bfea6aba7459ca3218452f85402eb0d810006487
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-29 12:14:42 +0300

    feat: implement loading popup section

--- frontend-web-react/src/components/Popup.tsx
+++ frontend-web-react/src/components/Popup.tsx
@@ -1,8 +1,22 @@
+import { Comment } from "react-loader-spinner";
 import { popupComponentInterface } from "../interfaces/components/components.interfaces";
 
 const Popup: React.FC<popupComponentInterface> = (props) => {
   if (!props.seen) return <></>;
-  return props.seen && <div></div>;
+  return (
+    props.seen && (
+      <div className="h-full w-full flex justify-center items-center bg-black bg-opacity-25 fixed backdrop-blur-sm">
+        {props.for === "loading" && (
+          <Comment
+            height="80"
+            width="80"
+            color="#fff"
+            backgroundColor="#0C7FF2"
+          />
+        )}
+      </div>
+    )
+  );
 };
 
 export default Popup;
