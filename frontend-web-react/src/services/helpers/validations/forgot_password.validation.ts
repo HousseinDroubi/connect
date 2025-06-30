af62c86120ec6b1363e746fe37b30e2e460e1e96
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-30 22:56:40 +0300

    feat: implement forgot password validation and showing validation for it

---
+++ frontend-web-react/src/services/helpers/validations/forgot_password.validation.ts
@@ -0,0 +1,43 @@
+import Joi from "joi";
+import { createAccountBodyInterface } from "../../../interfaces/requests/create_account_request";
+import { showPopupText } from "../popup_helper";
+import { SetPopupType } from "../../../interfaces/general_types";
+import { createAccountRequestValidationError } from "../../../interfaces/validations_responses/create_account_validtion_responses";
+import { forgotPasswordRequestValidationError } from "../../../interfaces/validations_responses/forgot_password_validtion_responses";
+import { forotPasswordBodyInterface } from "../../../interfaces/requests/forgot_password_request";
+
+const showValidationForForgotPasswordRequest = (
+  setPopupProps: SetPopupType,
+  error: forgotPasswordRequestValidationError
+) => {
+  switch (error) {
+    case "email_is_not_allowed_to_be_empty":
+    case "email_is_required":
+      showPopupText(setPopupProps, "Email is required");
+      break;
+    case "email_must_be_of_type_string":
+      showPopupText(setPopupProps, "Email must be of type text");
+      break;
+    case "invalid_email":
+      showPopupText(setPopupProps, "Invalid email");
+      break;
+  }
+};
+
+const validateForgotPassword = (data: forotPasswordBodyInterface) => {
+  const schema = Joi.object({
+    email: Joi.string()
+      .required()
+      .label("Email")
+      .email({ tlds: false })
+      .messages({
+        "any.required": "email_is_required",
+        "string.base": "email_must_be_of_type_string",
+        "string.email": "invalid_email",
+        "string.empty": "email_is_not_allowed_to_be_empty",
+      }),
+  });
+  return schema.validate(data).error?.details[0].message;
+};
+
+export { validateForgotPassword, showValidationForForgotPasswordRequest };
