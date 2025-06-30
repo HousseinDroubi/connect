7f3ecd727be824548090d932df726aff9d32f6eb
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-01 00:40:32 +0300

    feat: add query client provider and add react query dev tools

--- frontend-web-react/src/index.tsx
+++ frontend-web-react/src/index.tsx
@@ -3,10 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+
+const queryClient = new QueryClient();
+
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools />
+    <App />
+  </QueryClientProvider>
+);
 
 reportWebVitals();
