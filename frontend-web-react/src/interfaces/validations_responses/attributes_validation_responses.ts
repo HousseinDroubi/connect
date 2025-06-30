15f39e0a3e07353fb9986cf586f901ac193df05f
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-30 13:15:11 +0300

    chore: update way of exporting image

--- frontend-web-react/src/interfaces/validations_responses/attributes_validation_responses.ts
+++ frontend-web-react/src/interfaces/validations_responses/attributes_validation_responses.ts
@@ -1,19 +1,19 @@
-export type imageValidationError = "image_is_required";
+type imageValidationError = "image_is_required";
 
-export type emailValidationError =
+type emailValidationError =
   | "email_is_required"
   | "email_must_be_of_type_string"
   | "invalid_email"
   | "email_is_not_allowed_to_be_empty";
 
-export type passwordValidationError =
+type passwordValidationError =
   | "password_must_be_of_type_string"
   | "password_is_required"
   | "password_is_not_allowed_to_be_empty"
   | "password_must_be_minimum_5_digits"
   | "password_must_be_maximum_20_digits";
 
-export type confirmationPasswordValidationError =
+type confirmationPasswordValidationError =
   | "confirmation_password_must_be_of_type_string"
   | "confirmation_password_is_required"
   | "confirmation_password_is_not_allowed_to_be_empty"
@@ -21,15 +21,24 @@ export type confirmationPasswordValidationError =
   | "confirmation_password_must_be_maximum_20_digits"
   | "password_must_equal_to_confirmation_password";
 
-export type pinValidationError =
+type pinValidationError =
   | "pin_must_be_of_type_string"
   | "pin_must_be_exactly_6_digits"
   | "pin_is_required"
   | "pin_is_not_allowed_to_be_empty";
 
-export type usernameValidationError =
+type usernameValidationError =
   | "username_must_be_of_type_string"
   | "username_is_required"
   | "username_is_not_allowed_to_be_empty"
   | "username_must_be_minimum_3_digits"
   | "username_must_be_maximum_10_digits";
+
+export type {
+  imageValidationError,
+  emailValidationError,
+  passwordValidationError,
+  confirmationPasswordValidationError,
+  pinValidationError,
+  usernameValidationError,
+};
