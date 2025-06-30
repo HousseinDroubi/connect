2cf4109bad2895a80cc7a0d2e9657fc44492ec09
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-30 21:03:50 +0300

    refactor: update page first look

--- frontend-web-react/src/pages/VerifyAccount.tsx
+++ frontend-web-react/src/pages/VerifyAccount.tsx
@@ -2,20 +2,37 @@ import TitleBig from "../components/TitleBig";
 import { useParams } from "react-router-dom";
 import DoneIcon from "../assets/done.png";
 import ErrorIcon from "../assets/error.png";
-import { useEffect } from "react";
+import WaitingIcon from "../assets/waiting.png";
+import { useEffect, useState } from "react";
+import { verifyAccountPageStyleInterface } from "../interfaces/pages/verify_account";
 
 const VerifyAccount = () => {
   const { token } = useParams();
-  useEffect(() => {
-    console.log(token);
-  }, []);
+  const [pageStyle, setPageStyle] = useState<verifyAccountPageStyleInterface>({
+    result: "waiting",
+    content: "Verifying token...",
+  });
+
+  const verifyAccount = async () => {};
+
+  useEffect(() => {}, []);
 
   return (
     <div className="h-screen w-full flex flex-col items-center justify-center">
       <TitleBig title="Verify your account" />
-      <section className="flex flex-col items-center justify-center w-fit h-72">
-        <img src={DoneIcon} height={200} width={200} />
-        <p>Your account has been veified. You can login now</p>
+      <section className="flex flex-col items-center justify-center w-fit h-80">
+        <img
+          src={
+            pageStyle.result === "waiting"
+              ? WaitingIcon
+              : pageStyle.result === "done"
+              ? DoneIcon
+              : ErrorIcon
+          }
+          height={150}
+          width={150}
+        />
+        <p className="mt-8">{pageStyle.content}</p>
       </section>
     </div>
   );
