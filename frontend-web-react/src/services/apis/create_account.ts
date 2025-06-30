17b4da9ded168a84f1d94c1f9eb6679fda098129
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-06-30 14:32:52 +0300

    refactor: update axios interface

--- frontend-web-react/src/services/apis/create_account.ts
+++ frontend-web-react/src/services/apis/create_account.ts
@@ -1,7 +1,16 @@
 import axios, { AxiosResponse } from "axios";
 import { API_CREATE_ACCOUNT } from "../../constants/urls/auth_urls";
+import { createAccountResponseInterface } from "../../interfaces/responses/create_account_response";
+import { createAccountBodyInterface } from "../../interfaces/requests/create_account_request";
 
-const createAccountApi = async (formData: FormData): Promise<AxiosResponse> => {
+const createAccountApi = async (
+  formData: FormData
+): Promise<
+  AxiosResponse<
+    createAccountResponseInterface,
+    Omit<createAccountBodyInterface, "confirmation_password">
+  >
+> => {
   const response = await axios.post(API_CREATE_ACCOUNT, formData);
   return response;
 };
