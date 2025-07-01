3457e58f565f996d32844429d1c8997163deb143
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-01 22:44:19 +0300

    refactor: add interface for api params

---
+++ frontend-web-react/src/interfaces/services/apis/update_profile_data_api_param.ts
@@ -0,0 +1,6 @@
+interface updateProfileDataApiParamInterface {
+  formData: FormData;
+  token: string;
+}
+
+export { updateProfileDataApiParamInterface };
--- frontend-web-react/src/services/apis/update_profile_data.ts
+++ frontend-web-react/src/services/apis/update_profile_data.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { API_UPDATE_PROFILE_DATA } from "../../constants/urls/auth_urls";
 import { updateProfileDataResponseInterface } from "../../interfaces/responses/update_profile_data_response";
+import { updateProfileDataApiParamInterface } from "../../interfaces/services/apis/update_profile_data_api_param";
 
-const updateProfileDataApi = async (
-  formData: FormData,
-  token: string
-): Promise<updateProfileDataResponseInterface> => {
+const updateProfileDataApi = async ({
+  formData,
+  token,
+}: updateProfileDataApiParamInterface): Promise<updateProfileDataResponseInterface> => {
   const response = await axios.put(API_UPDATE_PROFILE_DATA, formData, {
     headers: { Authorization: `Bearer ${token}` },
   });
