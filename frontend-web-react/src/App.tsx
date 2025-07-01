81c4bbd2f38a09f8dc183668ead245599f9d3d1f
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-01 14:59:29 +0300

    feat: add urls for app main pages

--- frontend-web-react/src/App.tsx
+++ frontend-web-react/src/App.tsx
@@ -4,12 +4,27 @@ import CreateAccount from "./pages/CreateAccount";
 import VerifyAccount from "./pages/VerifyAccount";
 import ForgotPassword from "./pages/ForgotPassword";
 import UpdateForgottenPassword from "./pages/UpdateForgottenPassword";
+import Landing from "./pages/Landing";
+import Search from "./pages/Search";
+import Profile from "./pages/Profile";
+import Cnoversation from "./pages/Conversation";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/create_new_account" element={<CreateAccount />} />
+        <Route path="/verify_account/:token" element={<VerifyAccount />} />
+        <Route path="/forgot_password" element={<ForgotPassword />} />
+        <Route
+          path="/update_forgotten_password/:token"
+          element={<UpdateForgottenPassword />}
+        />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/conversation/:id" element={<Cnoversation />} />
         <Route path="/verify_account/:token" element={<VerifyAccount />} />
         <Route path="/forgot_password" element={<ForgotPassword />} />
         <Route
