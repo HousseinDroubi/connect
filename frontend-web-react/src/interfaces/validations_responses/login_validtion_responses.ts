8a342e28746b42047d5d98d1c850d6acef9a17b5
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-02 00:50:10 +0300

    refactor: update login validation responses

--- frontend-web-react/src/interfaces/validations_responses/login_validtion_responses.ts
+++ frontend-web-react/src/interfaces/validations_responses/login_validtion_responses.ts
@@ -1,9 +1,12 @@
 import {
   emailValidationError,
   passwordValidationError,
+  pinValidationError,
 } from "./attributes_validation_responses";
 
 export type loginRequestValidationError =
-  | emailValidationError
+  | Exclude<emailValidationError, "email_is_required">
+  | Exclude<pinValidationError, "pin_is_required">
   | passwordValidationError
+  | "either_email_or_pin_is_required"
   | undefined;
