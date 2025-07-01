ad74b0d44c73cc488bd45e6ec3c449a1aa49c93d
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-01 23:00:14 +0300

    feat: implement update password request and response interfaces

---
+++ frontend-web-react/src/interfaces/requests/update_password_request.ts
@@ -0,0 +1,7 @@
+interface updatePasswordBodyInterface {
+  old_password: string;
+  new_password: string;
+  confirmation_new_password: string;
+}
+
+export type { updatePasswordBodyInterface };
---
+++ frontend-web-react/src/interfaces/responses/update_password_response.ts
@@ -0,0 +1,5 @@
+interface updatePasswordResponseInterface {
+  result: "password_updated";
+}
+
+export type { updatePasswordResponseInterface };
