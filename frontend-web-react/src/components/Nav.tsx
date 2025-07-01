fdace9f8c080bc5f0f2f40e09e3618ef8ea1d51b
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-01 16:03:48 +0300

    feat: implement nav style

--- frontend-web-react/src/components/Nav.tsx
+++ frontend-web-react/src/components/Nav.tsx
@@ -1,5 +1,32 @@
+import Logo from "./Logo";
+import Title from "./Title";
+import SearchIcon from "../assets/search.png";
+import MessageIcon from "../assets/message.png";
+import ProfileIcon from "../assets/profile_default.png";
+
 const Nav = () => {
-  return <nav></nav>;
+  return (
+    <nav className="flex flex-col">
+      <article className="flex justify-between pl-8 pr-8 pt-5">
+        <section className="flex items-center w-36 justify-between">
+          <Title title="Connect" size="medium" />
+          <Logo />
+        </section>
+        <section className="flex items-center w-40 justify-between">
+          <div className="cursor-pointer size-9">
+            <img src={MessageIcon} alt="message" title="conversations" />
+          </div>
+          <div className="cursor-pointer size-9">
+            <img src={SearchIcon} alt="search" title="Find someone" />
+          </div>
+          <div className="cursor-pointer size-10 rounded-full">
+            <img src={ProfileIcon} alt="profile" title="Visit profile" />
+          </div>
+        </section>
+      </article>
+      <hr className="mt-2 bg-black" />
+    </nav>
+  );
 };
 
 export default Nav;
