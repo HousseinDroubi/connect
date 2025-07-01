0cde3b6b146c6111921bc1b4f2a335f522cdfd06
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-01 14:43:37 +0300

    feat: hanlde request on error case

--- frontend-web-react/src/services/hooks/mutations/login_mutation.ts
+++ frontend-web-react/src/services/hooks/mutations/login_mutation.ts
@@ -2,15 +2,39 @@ import { useMutation } from "@tanstack/react-query";
 import { loginApi } from "../../apis/login";
 import { loginBodyInterface } from "../../../interfaces/requests/login_request";
 import { loginResponseInterface } from "../../../interfaces/responses/login_response";
+import axios from "axios";
+import { showPopupText } from "../../helpers/popup_helper";
+import { SetPopupType } from "../../../interfaces/general_types";
 
-const useLogin = () =>
+const useLogin = (setPopupProps: SetPopupType) =>
   useMutation<loginResponseInterface, Error, loginBodyInterface>({
     mutationFn: loginApi,
     onSuccess(data) {
       console.log(data);
     },
     onError(error) {
-      console.warn(error);
+      if (axios.isAxiosError(error)) {
+        if (error.code === "ERR_NETWORK") {
+          showPopupText(setPopupProps, "Server isn't available");
+          return;
+        }
+
+        if (error.status === 401) {
+          showPopupText(setPopupProps, "Wrong email or password");
+          return;
+        } else if (error.status === 405) {
+          if (error.response?.data.result === "user_not_verified") {
+            showPopupText(
+              setPopupProps,
+              "You are not verified yet. Please open your inbox and activate your account"
+            );
+          } else if (error.response?.data.result === "user_account_deleted") {
+            showPopupText(setPopupProps, "Use account deleted");
+          }
+          return;
+        }
+      }
+      showPopupText(setPopupProps, "Something went wrong");
     },
   });
 
