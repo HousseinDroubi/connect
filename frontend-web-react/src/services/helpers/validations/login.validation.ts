b86544021496a52b867a2c60d777e4b91af01a5a
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-02 00:52:55 +0300

    refactor: update validate login and showing the error message

--- frontend-web-react/src/services/helpers/validations/login.validation.ts
+++ frontend-web-react/src/services/helpers/validations/login.validation.ts
@@ -1,19 +1,25 @@
 import Joi from "joi";
-import { createAccountBodyInterface } from "../../../interfaces/requests/create_account_request";
 import { showPopupText } from "../popup_helper";
 import { SetPopupType } from "../../../interfaces/general_types";
-import { createAccountRequestValidationError } from "../../../interfaces/validations_responses/create_account_validtion_responses";
-import { loginRequestValidationError } from "../../../interfaces/validations_responses/login_validtion_responses";
 import { loginBodyInterface } from "../../../interfaces/requests/login_request";
+import { loginRequestValidationError } from "../../../interfaces/validations_responses/login_validtion_responses";
 
 const showValidationForLoginRequest = (
   setPopupProps: SetPopupType,
   error: loginRequestValidationError
 ) => {
   switch (error) {
+    case "pin_must_be_of_type_string":
+      showPopupText(setPopupProps, "Pin must be of type string");
+      break;
+    case "pin_must_be_exactly_6_digits":
+      showPopupText(setPopupProps, "Pin must be exactly 6 digits");
+      break;
+    case "pin_is_not_allowed_to_be_empty":
+      showPopupText(setPopupProps, "Pin is not allowed to be empty");
+      break;
     case "email_is_not_allowed_to_be_empty":
-    case "email_is_required":
-      showPopupText(setPopupProps, "Email is required");
+      showPopupText(setPopupProps, "Email is not allowed to be empty");
       break;
     case "email_must_be_of_type_string":
       showPopupText(setPopupProps, "Email must be of type text");
@@ -35,20 +41,27 @@ const showValidationForLoginRequest = (
     case "password_must_be_of_type_string":
       showPopupText(setPopupProps, "Password must be of type text");
       break;
+    case "either_email_or_pin_is_required":
+      showPopupText(setPopupProps, "Either enter email or pin");
+      break;
   }
 };
 
 const validateLogin = (data: loginBodyInterface) => {
   const schema = Joi.object({
-    email: Joi.string()
-      .required()
-      .label("Email")
-      .email({ tlds: false })
+    email: Joi.string().label("Email").email({ tlds: false }).messages({
+      "any.required": "email_is_required",
+      "string.base": "email_must_be_of_type_string",
+      "string.email": "invalid_email",
+      "string.empty": "email_is_not_allowed_to_be_empty",
+    }),
+    pin: Joi.string()
+      .label("Pin")
+      .pattern(/^\d{6}$/)
       .messages({
-        "any.required": "email_is_required",
-        "string.base": "email_must_be_of_type_string",
-        "string.email": "invalid_email",
-        "string.empty": "email_is_not_allowed_to_be_empty",
+        "string.base": "pin_must_be_of_type_string",
+        "string.pattern.base": "pin_must_be_exactly_6_digits",
+        "string.empty": "pin_is_not_allowed_to_be_empty",
       }),
     password: Joi.string()
       .required()
@@ -62,7 +75,11 @@ const validateLogin = (data: loginBodyInterface) => {
         "string.min": "password_must_be_minimum_5_digits",
         "string.max": "password_must_be_maximum_20_digits",
       }),
-  });
+  })
+    .or("email", "pin")
+    .messages({
+      "object.missing": "either_email_or_pin_is_required",
+    });
 
   return schema.validate(data).error?.details[0].message;
 };
