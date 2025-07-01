34c853c7dac7a2d51ed6c00432b821533be9cd26
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-01 22:30:43 +0300

    refactor: update interface

--- frontend-web-react/src/interfaces/responses/update_profile_data_response.ts
+++ frontend-web-react/src/interfaces/responses/update_profile_data_response.ts
@@ -1,6 +1,7 @@
 interface updateProfileDataResponseInterface {
   result: "data_updated";
   new_profile_url?: string;
+  new_username?: string;
 }
 
 export type { updateProfileDataResponseInterface };
