24f55ede064ca8b7d2f04539b203e973d06a880e
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-01 22:36:02 +0300

    refactor: add token to headers

--- frontend-web-react/src/services/apis/update_profile_data.ts
+++ frontend-web-react/src/services/apis/update_profile_data.ts
@@ -1,11 +1,14 @@
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 import { API_UPDATE_PROFILE_DATA } from "../../constants/urls/auth_urls";
 import { updateProfileDataResponseInterface } from "../../interfaces/responses/update_profile_data_response";
 
 const updateProfileDataApi = async (
-  formData: FormData
+  formData: FormData,
+  token: string
 ): Promise<updateProfileDataResponseInterface> => {
-  const response = await axios.put(API_UPDATE_PROFILE_DATA, formData);
+  const response = await axios.put(API_UPDATE_PROFILE_DATA, formData, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
   return response.data;
 };
 
