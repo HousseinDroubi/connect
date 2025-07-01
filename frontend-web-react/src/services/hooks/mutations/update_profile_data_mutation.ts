7cb6a6d9cdad4054da36ebe81cf8d9b4924762e1
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-01 22:47:50 +0300

    feat: implement use profile data on success case

--- frontend-web-react/src/services/hooks/mutations/update_profile_data_mutation.ts
+++ frontend-web-react/src/services/hooks/mutations/update_profile_data_mutation.ts
@@ -7,15 +7,36 @@ import { showPopupText } from "../../helpers/popup_helper";
 import { SetPopupType } from "../../../interfaces/general_types";
 import { queryClient } from "../../..";
 import { updateProfileDataResponseInterface } from "../../../interfaces/responses/update_profile_data_response";
-import { useNavigate } from "react-router-dom";
+import { NavigateFunction, useNavigate } from "react-router-dom";
 import { updateProfileDataApi } from "../../apis/update_profile_data";
-const navigate = useNavigate();
+import { updateProfileDataApiParamInterface } from "../../../interfaces/services/apis/update_profile_data_api_param";
 
-const useUpdateProfileData = (setPopupProps: SetPopupType) =>
-  useMutation<updateProfileDataResponseInterface, Error, FormData>({
+const useUpdateProfileData = (
+  setPopupProps: SetPopupType,
+  navigate: NavigateFunction
+) =>
+  useMutation<
+    updateProfileDataResponseInterface,
+    Error,
+    updateProfileDataApiParamInterface
+  >({
     mutationFn: updateProfileDataApi,
     onSuccess(data) {
-      // Success
+      if (data.result === "data_updated") {
+        if (data.new_profile_url || data.new_username) {
+          const existingUserData = queryClient.getQueryData<any>(["user_data"]);
+
+          queryClient.setQueryData(["user_data"], {
+            ...existingUserData,
+            ...(data.new_profile_url && {
+              profile_url: data.new_profile_url,
+            }),
+            ...(data.new_username && { username: data.new_username }),
+          });
+        }
+        return;
+      }
+      throw new Error();
     },
     onError(error) {
       if (axios.isAxiosError(error)) {
