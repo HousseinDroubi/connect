9a9b626b6995e4737303b64e9ad4da0a579c135e
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-01 16:31:31 +0300

    feat: get user data and pass profile url to nav

--- frontend-web-react/src/pages/Search.tsx
+++ frontend-web-react/src/pages/Search.tsx
@@ -1,5 +1,19 @@
+import { useEffect } from "react";
+import Nav from "../components/Nav";
+import useUserData from "../services/hooks/queries/user_data_query";
+import { useNavigate } from "react-router-dom";
+
 const Search = () => {
-  return <></>;
+  const navigate = useNavigate();
+  const { data } = useUserData();
+  useEffect(() => {
+    if (data === null) navigate("/");
+  }, [data]);
+  return (
+    <>
+      <Nav profile_url={data?.profile_url} />
+    </>
+  );
 };
 
 export default Search;
