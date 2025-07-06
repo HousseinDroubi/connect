b2e0bdd100768168a13031845e99675e0d720726
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-06 15:53:45 +0300

    feat: implement search for users middleware validation

--- backend-nodejs/src/middlewares/validations/user.validation.middleware.ts
+++ backend-nodejs/src/middlewares/validations/user.validation.middleware.ts
@@ -1,5 +1,8 @@
 import { Request, Response, NextFunction } from "express";
-import { validateViewOtherUserProfile } from "../../validations/user.validation";
+import {
+  validateSearchForUsers,
+  validateViewOtherUserProfile,
+} from "../../validations/user.validation";
 import { isObjectIdValid } from "../../functions/general";
 
 const viewOtherUserProfileValidationMiddleware = (
@@ -28,4 +31,27 @@ const viewOtherUserProfileValidationMiddleware = (
   next();
 };
 
-export { viewOtherUserProfileValidationMiddleware };
+const searchForUsersValidationMiddleware = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  // Get request body
+  const { content } = request.params;
+
+  // Validate request params
+  const error = validateSearchForUsers(content).error?.details[0].message;
+  if (error) {
+    return response.status(400).json({
+      result: "validation_error",
+      error,
+    });
+  }
+
+  next();
+};
+
+export {
+  viewOtherUserProfileValidationMiddleware,
+  searchForUsersValidationMiddleware,
+};
