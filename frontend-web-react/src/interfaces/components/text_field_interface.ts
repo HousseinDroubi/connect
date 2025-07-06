8f8750a9a95a6445a854e112ecd1ce205ce35801
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-06 14:42:19 +0300

    refactor: add is full property for full width

--- frontend-web-react/src/interfaces/components/text_field_interface.ts
+++ frontend-web-react/src/interfaces/components/text_field_interface.ts
@@ -4,5 +4,6 @@ interface textFieldComponentInterface {
   is_password?: boolean;
   value: string;
   setText: React.Dispatch<React.SetStateAction<string>>;
+  is_full?: boolean;
 }
 export type { textFieldComponentInterface };
