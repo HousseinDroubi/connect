5a6864529c30f985e62c0438f041414f9fdaaec2
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-06 11:13:08 +0300

    feat: initialize other user component

---
+++ frontend-web-react/src/components/OtherUser.tsx
@@ -0,0 +1,7 @@
+import { otherUserComponentInterface } from "../interfaces/components/other_user_interface";
+
+const OtherUser: React.FC<otherUserComponentInterface> = (props) => {
+  return <></>;
+};
+
+export default OtherUser;
