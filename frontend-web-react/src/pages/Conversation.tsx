99bbf2d8b41acb7097cbe4de9c11b287dec9049f
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-07 00:17:28 +0300

    feat: implement main page style

--- frontend-web-react/src/pages/Conversation.tsx
+++ frontend-web-react/src/pages/Conversation.tsx
@@ -6,13 +6,18 @@ import { useNavigate } from "react-router-dom";
 const Conversation = () => {
   const navigate = useNavigate();
   const { data } = useUserData();
+
   useEffect(() => {
     if (data === null) navigate("/");
   }, [data]);
+
   return (
-    <>
+    <div className="h-screen w-full flex flex-col">
       <Nav profile_url={data?.profile_url} />
-    </>
+      <div className="w-full h-full flex justify-center">
+        <section className="w-3/4 h-full mt-10"></section>
+      </div>
+    </div>
   );
 };
 
