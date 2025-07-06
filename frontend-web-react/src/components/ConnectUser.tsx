481dda89b5326a36b296d2750352434bff4f3641
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-06 16:54:11 +0300

    refactor: update connect user main container style

--- frontend-web-react/src/components/ConnectUser.tsx
+++ frontend-web-react/src/components/ConnectUser.tsx
@@ -4,7 +4,7 @@ import Title from "./Title";
 
 const ConnectUser: React.FC<connectUserComponentInterface> = (props) => {
   return (
-    <article className="flex flex-col">
+    <article className="flex flex-col cursor-pointer">
       <div className="flex h-32 items-center w-full">
         <section
           className={`size-24 ml-4 ${!props.is_group && "rounded-full"}`}
