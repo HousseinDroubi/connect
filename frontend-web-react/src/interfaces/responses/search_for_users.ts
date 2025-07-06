62c75d02a7a75518d995b3eb1d5ec8a79132ebe9
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-06 16:31:25 +0300

    refactor: update serch for users api response interface

--- frontend-web-react/src/interfaces/responses/search_for_users.ts
+++ frontend-web-react/src/interfaces/responses/search_for_users.ts
@@ -1,8 +1,10 @@
 interface searchForUsersResponseInterface {
-  _id: string;
-  pin: string;
-  profile_url: string;
-  username: string;
+  users: Array<{
+    _id: string;
+    pin: string;
+    profile_url: string;
+    username: string;
+  }>;
 }
 
 export { searchForUsersResponseInterface };
