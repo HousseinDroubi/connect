b03f17235df5f76e85c9d98ede6feb5686c5f247
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-06 13:28:14 +0300

    feat: implement connect user component interface

--- frontend-web-react/src/components/ConnectUser.tsx
+++ frontend-web-react/src/components/ConnectUser.tsx
@@ -1,7 +1,51 @@
 import { connectUserComponentInterface } from "../interfaces/components/other_user_interface";
+import GroupIcon from "../assets/group.png";
+import Title from "./Title";
 
 const ConnectUser: React.FC<connectUserComponentInterface> = (props) => {
-  return <></>;
+  return (
+    <div className="flex h-32 items-center w-full bg-violet-100">
+      <section className={`size-24 ml-4 ${!props.is_group && "rounded-full"}`}>
+        <img
+          src={props.is_group ? GroupIcon : props.profile_url}
+          alt=""
+          className={`size-full ${!props.is_group && "rounded-full"}`}
+        />
+      </section>
+      <section className="flex flex-col items-start justify-center h-full ml-4">
+        <Title
+          size="medium"
+          title={props.is_group ? "Connected Users" : props.username!}
+        />
+        {props.for === "status" &&
+          (props.is_group ? (
+            <p className="italic">All connect users</p>
+          ) : (
+            <section className="flex items-center mt-2">
+              <div
+                className={`rounded-full size-3 ${
+                  props.status ? "bg-green-600" : "bg-red-600"
+                }`}
+              ></div>
+              <p className="ml-2">{props.status ? "Online" : "Offline"}</p>
+            </section>
+          ))}
+        {props.for === "conversation" &&
+          (!props.last_message_text ? (
+            <p className="italic">No messages yet</p>
+          ) : (
+            <p className="mt-2 font-medium">
+              {!props.is_group
+                ? props.last_message_text
+                : `${props.last_message_receipt}: ${props.last_message_text}`}
+            </p>
+          ))}
+        {props.for === "search" && (
+          <p className="mt-2 font-medium">{`pin: #${props.pin}`}</p>
+        )}
+      </section>
+    </div>
+  );
 };
 
 export default ConnectUser;
