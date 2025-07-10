e326cef32386973fdc2db58d33ba42608bb01a98
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-10 22:42:48 +0300

    refactor: update interface - add set image set state

--- frontend-web-react/src/interfaces/components/text_field_interface.ts
+++ frontend-web-react/src/interfaces/components/text_field_interface.ts
@@ -4,6 +4,7 @@ interface textFieldComponentInterface {
   is_password?: boolean;
   value: string;
   setText: React.Dispatch<React.SetStateAction<string>>;
+  setImage?: React.Dispatch<React.SetStateAction<File | null>>;
   is_full?: boolean;
   is_for_message?: boolean;
   doNextFunction: (() => any) | null;
