f1365b2d6061c9e4d467fee909559d8805a13fb4
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-11 01:44:06 +0300

    feat: implement view image api

---
+++ frontend-web-react/src/services/apis/message/view_image.ts
@@ -0,0 +1,17 @@
+import axios, { AxiosResponse } from "axios";
+import { viewImageApiParamInterface } from "../../../interfaces/services/apis/view_image_api_param";
+import { API_VIEW_IMAGE } from "../../../constants/urls/message_urls";
+
+const viewImageApi = async ({
+  message_id,
+  token,
+}: viewImageApiParamInterface): Promise<
+  AxiosResponse<viewImageApiParamInterface, string>
+> => {
+  const response = await axios.get(`${API_VIEW_IMAGE}/${message_id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return response;
+};
+
+export { viewImageApi };
