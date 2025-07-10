1af74fc50fa7d852d2da89bae7615da7615b4406
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-11 01:37:10 +0300

    refactor: update message component interface

--- frontend-web-react/src/interfaces/components/message_interface.ts
+++ frontend-web-react/src/interfaces/components/message_interface.ts
@@ -2,6 +2,7 @@ interface messageComponentInterface {
   is_text: boolean;
   is_left: boolean;
   content: string;
+  token: string;
   group_user?: {
     profile_url: string;
     username: string;
