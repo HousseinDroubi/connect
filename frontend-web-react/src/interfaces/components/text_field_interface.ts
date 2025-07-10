99c3a7e6ce23a2e593f7754460f3e6504bc6303d
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-10 21:59:08 +0300

    refactor: update text field interface

--- frontend-web-react/src/interfaces/components/text_field_interface.ts
+++ frontend-web-react/src/interfaces/components/text_field_interface.ts
@@ -1,10 +1,11 @@
 interface textFieldComponentInterface {
-  title: string;
+  title?: string;
   hint: string;
   is_password?: boolean;
   value: string;
   setText: React.Dispatch<React.SetStateAction<string>>;
   is_full?: boolean;
+  is_for_message?: boolean;
   doNextFunction: (() => any) | null;
 }
 export type { textFieldComponentInterface };
