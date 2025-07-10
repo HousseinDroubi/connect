7a5cb1691cb0648b62ca85aace800d4739f32963
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-10 21:50:57 +0300

    feat: add connect user component

--- frontend-web-react/src/pages/Conversation.tsx
+++ frontend-web-react/src/pages/Conversation.tsx
@@ -25,7 +25,27 @@ const Conversation = () => {
     <div className="h-screen w-full flex flex-col">
       <Nav profile_url={data?.profile_url} />
       <div className="w-full h-full flex justify-center">
-        <section className="w-3/4 h-full mt-10"></section>
+        <section className="w-3/4 h-full">
+          <ConnectUser
+            for="status"
+            is_group={getConversationMessagesData?.recipient === undefined}
+            profile_url={
+              !getConversationMessagesData?.recipient
+                ? undefined
+                : getConversationMessagesData?.recipient?.profile_url
+            }
+            username={
+              !getConversationMessagesData?.recipient
+                ? undefined
+                : getConversationMessagesData.recipient.username
+            }
+            status={
+              !getConversationMessagesData?.recipient
+                ? undefined
+                : getConversationMessagesData?.recipient?.is_online
+            }
+          />
+        </section>
       </div>
     </div>
   );
