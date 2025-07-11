6280a3c6d6c84be6f50c67f721998b384fbfd59c
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-11 11:35:59 +0300

    feat: implement launch ws connection function

--- frontend-web-react/src/services/messages/Singleton.ts
+++ frontend-web-react/src/services/messages/Singleton.ts
@@ -1,9 +1,20 @@
 class Singleton {
   private static instance: Singleton;
+  private static websocket: WebSocket;
 
-  public static getInstance(): Singleton {
+  private constructor(token: string) {
+    this.launchWsConnection(token);
+  }
+
+  private launchWsConnection(token: string) {
+    Singleton.websocket = new WebSocket(
+      `${process.env.REACT_APP_API_BASE_URL}?token=${token}`
+    );
+  }
+
+  public static getInstance(token: string): Singleton {
     if (!Singleton.instance) {
-      Singleton.instance = new Singleton();
+      Singleton.instance = new Singleton(token);
     }
     return Singleton.instance;
   }
