d45c07f9bf8804b543b8ff31cb2f45cb49bb1c74
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-18 23:52:08 +0300

    feat: implement ws requests interfaces

---
+++ frontend-web-react/src/interfaces/services/messages/requests.ts
@@ -0,0 +1,23 @@
+interface wsSendMessageRequestInterface {
+  event_name: "new_message";
+  is_text: boolean;
+  content: string;
+  to: string;
+}
+
+interface wsEditMessageRequestInterface {
+  event_name: "edit_message";
+  message_id: string;
+  message_new_content: string;
+}
+
+interface wsDeleteMessageRequestInterface {
+  event_name: "delete_message";
+  message_id: string;
+}
+
+export {
+  wsSendMessageRequestInterface,
+  wsEditMessageRequestInterface,
+  wsDeleteMessageRequestInterface,
+};
