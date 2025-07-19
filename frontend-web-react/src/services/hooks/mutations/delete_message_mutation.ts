a51c89639934bf5c1f2ef76ee4d6548a754bf15f
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-19 22:30:03 +0300

    feat: implement caching delete message

--- frontend-web-react/src/services/hooks/mutations/delete_message_mutation.ts
+++ frontend-web-react/src/services/hooks/mutations/delete_message_mutation.ts
@@ -6,6 +6,9 @@ import { deleteMessageApiParamInterface } from "../../../interfaces/services/api
 import axios from "axios";
 import { showPopupText } from "../../helpers/popup_helper";
 import { NavigateFunction } from "react-router-dom";
+import { queryClient } from "../../..";
+import { cloneDeep, update } from "lodash";
+import { getConversationMessagesResponseInterface } from "../../../interfaces/responses/get_conversation_message_response";
 
 const useDeleteMessage = (
   setPopupProps: SetPopupType,
@@ -18,7 +21,86 @@ const useDeleteMessage = (
   >({
     mutationFn: deleteMessageApi,
     onSuccess(data) {
-      console.log(data);
+      if (data.result === "message_deleted") {
+        const user_data: any = queryClient.getQueryData(["user_data"]);
+
+        if (!user_data || !user_data.conversations) {
+          return;
+        }
+
+        const conversations = queryClient.getQueriesData({
+          queryKey: ["conversations"],
+          exact: false,
+        });
+
+        let new_message;
+
+        conversations.map(([queryKey, data_cache]) => {
+          const conversation =
+            data_cache as getConversationMessagesResponseInterface;
+          if (conversation.conversation_id === data.message_conversation_id) {
+            const index = conversation.messages.findIndex(
+              (message) => message._id === data.message_id
+            );
+
+            if (index !== -1) {
+              const updatedMessages = [...conversation.messages];
+              updatedMessages.splice(index, 1);
+
+              const updated_conversation = {
+                ...conversation,
+                messages: updatedMessages,
+              };
+
+              queryClient.setQueryData(queryKey, updated_conversation);
+
+              if (updatedMessages.length !== 0) {
+                new_message = {
+                  _id: updatedMessages[updatedMessages.length - 1]._id,
+                  sender: updatedMessages[updatedMessages.length - 1].sender,
+                  receiver:
+                    updatedMessages[updatedMessages.length - 1].receiver,
+                  is_text: updatedMessages[updatedMessages.length - 1].is_text,
+                  content: updatedMessages[updatedMessages.length - 1].content,
+                  conversation_id:
+                    updatedMessages[updatedMessages.length - 1].conversation_id,
+                  created_at:
+                    updatedMessages[updatedMessages.length - 1].created_at,
+                  deleted:
+                    updatedMessages[updatedMessages.length - 1]
+                      .deleted_for_others_at !== null,
+                };
+              } else {
+                new_message = null;
+              }
+            }
+          }
+        });
+
+        const user_data_conversation_index = user_data.conversations.findIndex(
+          (conversation: any) =>
+            conversation._id === data.message_conversation_id
+        );
+
+        if (user_data_conversation_index !== -1) {
+          if (
+            user_data.conversations[user_data_conversation_index]
+              .last_message &&
+            user_data.conversations[user_data_conversation_index].last_message
+              ._id === data.message_id
+          ) {
+            const updated_user_data = cloneDeep(user_data);
+            updated_user_data.conversations[
+              user_data_conversation_index
+            ].last_message = new_message;
+
+            queryClient.setQueryData(["user_data"], updated_user_data);
+          }
+        }
+
+        return;
+      }
+      throw new Error();
     },
     onError(error) {
       if (axios.isAxiosError(error)) {
