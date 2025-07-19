d0148f88839f5da039a6f3712a138a0edd4e5255
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-19 11:50:40 +0300

    refactor: update message in-group style

--- frontend-web-react/src/components/Message.tsx
+++ frontend-web-react/src/components/Message.tsx
@@ -40,7 +40,7 @@ const Message: React.FC<messageComponentInterface> = ({
       )}
       <section
         className={`rounded-lg w-full flex flex-col items-start ${
-          !group_user && "justify-center"
+          group_user ? "justify-around" : "justify-center"
         } ${is_left ? "ml-2" : "mr-2"} ${
           is_text ? (group_user ? "min-h-20" : "min-h-16") : "h-72"
         } ${is_left ? "bg-ice_blue" : "bg-blue"} p-2`}
