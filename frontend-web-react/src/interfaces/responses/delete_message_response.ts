307aefa482a04a27b14cc53bd8e83972b4a97113
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-19 21:50:53 +0300

    refactor: update delete message response interface

--- frontend-web-react/src/interfaces/responses/delete_message_response.ts
+++ frontend-web-react/src/interfaces/responses/delete_message_response.ts
@@ -1,5 +1,7 @@
 interface deleteMessageResponseInterface {
   result: "message_deleted";
+  message_id: string;
+  message_conversation_id: string;
 }
 
 export type { deleteMessageResponseInterface };
