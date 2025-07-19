76d3231cb2cafa10d3df6ee1ed40542c2e34f42b
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-19 23:04:20 +0300

    feat: add upload image api

--- frontend-web-react/src/constants/urls/message_urls.ts
+++ frontend-web-react/src/constants/urls/message_urls.ts
@@ -4,5 +4,6 @@ const API_AUTH = `${API_BASE_URL}/message`;
 
 const API_VIEW_IMAGE = `${API_AUTH}/view_image`;
 const API_DELETE_MESSAGE = `${API_AUTH}/delete_message_for_sender`;
+const API_UPLOAD_IMAGE = `${API_AUTH}/upload_image`;
 
-export { API_VIEW_IMAGE, API_DELETE_MESSAGE };
+export { API_VIEW_IMAGE, API_DELETE_MESSAGE, API_UPLOAD_IMAGE };
