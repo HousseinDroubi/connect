475e5a22527226eb1fd0b00b90a75526b5c7cd19
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-19 21:41:48 +0300

    fix: update api url

--- frontend-web-react/src/services/apis/message/delete_message.ts
+++ frontend-web-react/src/services/apis/message/delete_message.ts
@@ -1,5 +1,8 @@
 import axios, { AxiosResponse } from "axios";
-import { API_VIEW_IMAGE } from "../../../constants/urls/message_urls";
+import {
+  API_DELETE_MESSAGE,
+  API_VIEW_IMAGE,
+} from "../../../constants/urls/message_urls";
 import { deleteMessageApiParamInterface } from "../../../interfaces/services/apis/delete_message_api_param";
 import { deleteMessageResponseInterface } from "../../../interfaces/responses/delete_message_response";
 
@@ -7,7 +10,7 @@ const deleteMessageApi = async ({
   message_id,
   token,
 }: deleteMessageApiParamInterface): Promise<deleteMessageResponseInterface> => {
-  const response = await axios.get(`${API_VIEW_IMAGE}/${message_id}`, {
+  const response = await axios.get(`${API_DELETE_MESSAGE}/${message_id}`, {
     headers: { Authorization: `Bearer ${token}` },
   });
 
