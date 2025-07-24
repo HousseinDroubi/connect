15d756b97eca5b0d2b8f6f94e41b9b80bcdf2755
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-24 17:48:42 +0300

    feat: implement profile widget config

---
+++ frontend-mobile-flutter/lib/views/widgets/profile/profile_widget_config.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+class ProfileWidgetConfig {
+  static const String imageDefaultSoucre = "assets/icons/profile_default.png";
+  File? _imageFile;
+  String? _imageSource;
+
+  ProfileWidgetConfig({File? imageFile, String? imageSource})
+    : _imageFile = imageFile,
+      _imageSource = imageSource;
+
+  File? get imageFile => _imageFile;
+  String? get imageSource => _imageSource;
+
+  set imageFile(File? imageFile) {
+    _imageFile = imageFile;
+  }
+
+  set imageSource(String? imageSource) {
+    _imageSource = imageSource;
+  }
+}
