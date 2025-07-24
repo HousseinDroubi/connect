1e71d57ba67571e8b2953e886866e8fe3d6a9701
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-24 14:43:54 +0300

    feat: implement create account style

--- frontend-mobile-flutter/lib/views/screens/create_account/create_account_screen.dart
+++ frontend-mobile-flutter/lib/views/screens/create_account/create_account_screen.dart
@@ -1,3 +1,10 @@
+import 'package:connect/views/screens/create_account/create_account_view_model.dart';
+import 'package:connect/views/widgets/button/button_widget.dart';
+import 'package:connect/views/widgets/button/button_widget_config.dart';
+import 'package:connect/views/widgets/text_field/text_field_widget_config.dart';
+import 'package:connect/views/widgets/text_field/text_field_widget_view.dart';
+import 'package:connect/views/widgets/title/title_widget.dart';
+import 'package:connect/views/widgets/title/title_widget_config.dart';
 import 'package:flutter/material.dart';
 
 class CreateAccount extends StatefulWidget {
@@ -8,8 +15,71 @@ class CreateAccount extends StatefulWidget {
 }
 
 class _CreateAccountState extends State<CreateAccount> {
+  CreateAccountViewModel viewModel = CreateAccountViewModel();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TitleWidget(config: TitleWidgetConfig(title: "Create New Account")),
+            SizedBox(height: 5),
+            TextFieldWidget(
+              config: TextFieldWidgetConfig(
+                title: "Email",
+                hint: "Enter your email",
+                nextFunction: () {},
+                textEditingController: viewModel.emailController,
+              ),
+            ),
+            SizedBox(height: 15),
+            TextFieldWidget(
+              config: TextFieldWidgetConfig(
+                title: "username",
+                hint: "Enter your username",
+                nextFunction: () {},
+                textEditingController: viewModel.usernameController,
+              ),
+            ),
+            SizedBox(height: 15),
+            TextFieldWidget(
+              config: TextFieldWidgetConfig(
+                title: "Pin",
+                hint: "Enter your pin",
+                nextFunction: () {},
+                textEditingController: viewModel.pinController,
+              ),
+            ),
+            SizedBox(height: 15),
+            TextFieldWidget(
+              config: TextFieldWidgetConfig(
+                title: "Password",
+                hint: "Enter your password",
+                nextFunction: () {},
+                textEditingController: viewModel.passwordController,
+              ),
+            ),
+            SizedBox(height: 15),
+            TextFieldWidget(
+              config: TextFieldWidgetConfig(
+                title: "Confirmation Password",
+                hint: "Re-enter your password",
+                nextFunction: () {},
+                textEditingController: viewModel.confirmationPasswordController,
+              ),
+            ),
+            SizedBox(height: 10),
+            ButtonWidget(
+              config: ButtonWidgetConifg(
+                buttonText: "Create new account",
+                buttonFn: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
