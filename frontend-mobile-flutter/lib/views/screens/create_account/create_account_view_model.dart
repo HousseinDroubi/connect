8b75d2eb4a5128a3c7f0513c57a370966a57a1a6
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-24 14:43:38 +0300

    feat: implement create account view model

---
+++ frontend-mobile-flutter/lib/views/screens/create_account/create_account_view_model.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+class CreateAccountViewModel {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _pinController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmationPasswordController =
+      TextEditingController();
+
+  TextEditingController get emailController => _emailController;
+  TextEditingController get usernameController => _usernameController;
+  TextEditingController get pinController => _pinController;
+  TextEditingController get passwordController => _passwordController;
+  TextEditingController get confirmationPasswordController =>
+      _confirmationPasswordController;
+}
