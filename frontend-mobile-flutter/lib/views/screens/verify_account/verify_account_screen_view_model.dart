9bf0e27b771f7ac0999a7a64e8b830729d50730e
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-25 22:24:01 +0300

    feat: implement verify account screen view model

---
+++ frontend-mobile-flutter/lib/views/screens/verify_account/verify_account_screen_view_model.dart
@@ -0,0 +1,13 @@
+class VerifyAccountScreenViewModel {
+  static const String doneIconPath = "assets/icons/done.png";
+  static const String errorIconPath = "assets/icons/error.png";
+  static const String waitingIconPath = "assets/icons/waiting.png";
+
+  void verifyAccount() {
+    // Todo
+  }
+
+  void navigateToLogin() {
+    // Todo
+  }
+}
