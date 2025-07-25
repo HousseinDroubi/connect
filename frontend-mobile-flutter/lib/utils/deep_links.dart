3db0f2b8d16e6e6907a73476fe377b8633dce29a
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-26 02:41:04 +0300

    feat: implement getting token and screen name from deep linking

---
+++ frontend-mobile-flutter/lib/utils/deep_links.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'package:app_links/app_links.dart';
+
+Future<Map<String, String>?> initDeepLinks(
+  AppLinks appLinks,
+  StreamSubscription<Uri>? sub,
+) async {
+  final initialLink = await appLinks.getInitialLink();
+  if (initialLink != null) {
+    return handleUri(initialLink);
+  }
+
+  sub = appLinks.uriLinkStream.listen((uri) {
+    handleUri(uri);
+  });
+  return null;
+}
+
+Map<String, String>? handleUri(Uri uri) {
+  Map<String, String> map = {};
+  if (uri.queryParameters.containsKey("token")) {
+    map["token"] = uri.queryParameters["token"] ?? "";
+    if (uri.pathSegments.last == "verify_account") {
+      map["to"] = "verify_account";
+    } else if (uri.pathSegments.last == "update_forgotten_password") {
+      map["to"] = "update_forgotten_password";
+    }
+  }
+  return null;
+}
