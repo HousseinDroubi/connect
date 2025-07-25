caf6e0d1d3c53725387646e0a12894f3ff360488
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-25 21:55:05 +0300

    feat: implement update forgotten password view model

---
+++ frontend-mobile-flutter/lib/views/screens/update_forgotten_password/update_forgotten_password_screen_view_model.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class UpdateForgottenPasswordScreenViewModel {
+  final TextEditingController _newPasswordController = TextEditingController();
+  final TextEditingController _confirmationNewPasswordController =
+      TextEditingController();
+
+  TextEditingController get newPasswordController => _newPasswordController;
+  TextEditingController get confirmationNewPasswordController =>
+      _confirmationNewPasswordController;
+}
