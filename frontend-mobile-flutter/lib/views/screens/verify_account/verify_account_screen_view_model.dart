75e8145f70d34f5caebcf3be99f6025c26ea5630
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-25 22:28:50 +0300

    refactor: add variables

--- frontend-mobile-flutter/lib/views/screens/verify_account/verify_account_screen_view_model.dart
+++ frontend-mobile-flutter/lib/views/screens/verify_account/verify_account_screen_view_model.dart
@@ -5,6 +5,8 @@ class VerifyAccountScreenViewModel {
   static const String doneIconPath = "assets/icons/done.png";
   static const String errorIconPath = "assets/icons/error.png";
   static const String waitingIconPath = "assets/icons/waiting.png";
+  bool isDone = false;
+  bool isError = false;
 
   void verifyAccount() {
     // Todo
