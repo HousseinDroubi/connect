1613ec4167496a1fa425c7c6d6f28e8606da9ce8
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-07-26 03:05:06 +0300

    refactor: update routes in main

--- frontend-mobile-flutter/lib/main.dart
+++ frontend-mobile-flutter/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:connect/views/screens/create_account/create_account.dart';
+import 'package:connect/views/screens/deep_linking/deep_linking.dart';
 import 'package:connect/views/screens/forgot_password/forgot_password.dart';
+import 'package:connect/views/screens/login/login_screen.dart';
 import 'package:connect/views/screens/update_forgotten_password/update_forgotten_password.dart';
 import 'package:connect/views/screens/verify_account/verify_account.dart';
 import 'package:flutter/material.dart';
-import "views/screens/login/login_screen.dart";
 
 void main() {
   runApp(const MyApp());
@@ -23,7 +24,8 @@ class _MyAppState extends State<MyApp> {
       title: "Connect",
       initialRoute: "/",
       routes: {
-        "/": (context) => LoginScreen(),
+        "/": (context) => DeepLinking(),
+        "/login": (context) => LoginScreen(),
         "/create_account": (context) => CreateAccount(),
         "/forgot_password": (context) => ForgotPassword(),
         "/verify_account": (context) => VerifyAccount(),
