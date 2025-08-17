5e3b4f3b74780e2ea6ecd16a2361898a729a34e5
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-08-17 23:13:09 +0300

    feat: implement create account request validation

---
+++ frontend-mobile-flutter/lib/utils/validate_requests.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+import 'package:connect/utils/validate_properties.dart';
+
+String? validateCreateAccountRequest({
+  required File? imageFile,
+  required String email,
+  required String username,
+  required String pin,
+  required String password,
+  required String confirmationPassowrd,
+}) {
+  List<String?> validations = [
+    validateImage(imageFile: imageFile),
+    validateEmail(email: email),
+    validateUsername(username: username),
+    validatePin(pin: pin),
+    validatePassword(password: password),
+    validatePassword(password: confirmationPassowrd),
+  ];
+
+  for (String? validationResult in validations) {
+    if (validationResult != null) return validationResult;
+  }
+
+  return null;
+}
