d0b5f4d805fdfd51494f13f5bacd55bdecfb13de
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-10-31 07:25:04 +0200

    feat: send and validate create account request

--- frontend-mobile-flutter/lib/views/screens/create_account/create_account_view_model.dart
+++ frontend-mobile-flutter/lib/views/screens/create_account/create_account_view_model.dart
@@ -1,6 +1,8 @@
-import 'package:connect/constants/my_colors.dart';
+import 'dart:io';
+import 'package:connect/utils/apis/auth_service.dart';
 import 'package:connect/utils/dialog.dart';
 import 'package:connect/utils/validate_requests.dart';
+import 'package:connect/views/screens/create_account/create_account_model.dart';
 import 'package:connect/views/widgets/profile/profile_widget_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -21,14 +23,20 @@ class CreateAccountViewModel {
       _confirmationPasswordController;
   ProfileWidgetConfig profileWidgetConfig = ProfileWidgetConfig();
 
-  void createAccountRequest(BuildContext context) {
+  void createAccountRequest(BuildContext context) async{
+    File? imageFile = profileWidgetConfig.imageFile;
+    String email = emailController.text;
+    String username = usernameController.text;
+    String pin = pinController.text;
+    String password = passwordController.text;
+    String confirmation_password = confirmationPasswordController.text;
     String? validationResult = validateCreateAccountRequest(
-      imageFile: profileWidgetConfig.imageFile,
-      email: emailController.text,
-      username: usernameController.text,
-      pin: pinController.text,
-      password: passwordController.text,
-      confirmationPassword: confirmationPasswordController.text,
+      imageFile: imageFile,
+      email: email,
+      username: username,
+      pin: pin,
+      password: password,
+      confirmationPassword: confirmation_password,
     );
     if (validationResult != null) {
       showPopup(
@@ -36,5 +44,29 @@ class CreateAccountViewModel {
       );
       return;
     }
+    showPopup(popupCase: PopupLoading(context: context));
+    final CreateAccountModel response = await AuthService().createAccount(imageFile: imageFile!, email: email, username: username, pin: pin, password: password);
+    hidePopup(context);
+    String popup_alert_message;
+    if(response.result!="account_created"){
+      if(response.result=="validation_error" && response.error=="invalid_email"){
+        popup_alert_message = "Invalid email";
+      }else if(response.result=="email_or_pin_taken"){
+        popup_alert_message = "email or pin is taken";
+      }else{
+        print(response.result);
+        print(response.error);
+        popup_alert_message = "Something went wrong";
+      }
+    }else{
+      imageFile = null;
+      emailController.text = "";
+      usernameController.text = "";
+      pinController.text = "";
+      passwordController.text = "";
+      confirmationPasswordController.text = "";
+      popup_alert_message = "Account created. Please go to your inbox and activate it.";
+    }
+    showPopup(popupCase: PopupAlert(context: context, popupContent: popup_alert_message));
   }
 }
