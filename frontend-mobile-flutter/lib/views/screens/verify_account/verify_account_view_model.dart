d5ea195df6a05ad3a6b616d0772241dcde66a968
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-11-02 06:29:49 +0200

    style: remove not used package

--- frontend-mobile-flutter/lib/views/screens/verify_account/verify_account_view_model.dart
+++ frontend-mobile-flutter/lib/views/screens/verify_account/verify_account_view_model.dart
@@ -1,6 +1,5 @@
 import 'package:connect/utils/apis/auth_service.dart';
 import 'package:connect/utils/dialog.dart';
-import 'package:connect/views/screens/login/login.dart';
 import 'package:connect/views/screens/verify_account/verify_account_model.dart';
 import 'package:flutter/material.dart';
 
