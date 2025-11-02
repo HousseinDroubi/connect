ffb030fa6fdde1302048befbf2d994756f262f61
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-11-02 06:21:56 +0200

    feat: implement forgot password api

--- frontend-mobile-flutter/lib/utils/apis/auth_service.dart
+++ frontend-mobile-flutter/lib/utils/apis/auth_service.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:connect/constants/urls.dart';
 import 'package:connect/views/screens/create_account/create_account_model.dart';
+import 'package:connect/views/screens/forgot_password/forgot_password_model.dart';
 import 'package:connect/views/screens/verify_account/verify_account_model.dart';
 import 'package:dio/dio.dart';
 
@@ -56,6 +57,23 @@ class AuthService {
       final String error = e.response?.data["error"] ?? e.message;
 
       return VerifyAccountModel.fromJson({"result": result, "error": error});
+    }
+  }
+
+  Future<ForgotPasswordModel> forgotPassword(String email) async {
+    try {
+      final String url = "$_base_url/forgot_password";
+      final response = await _dio.post(
+        url,
+        data: {"email": email},
+        options: Options(headers: {'Content-Type': 'application/json'}),
+      );
+      return ForgotPasswordModel.fromJson(response.data);
+    } on DioException catch (e) {
+      final String result = e.response?.data["result"] ?? "failed";
+      final String error = e.response?.data["error"] ?? e.message;
+
+      return ForgotPasswordModel.fromJson({"result": result, "error": error});
     }
   }
 }
