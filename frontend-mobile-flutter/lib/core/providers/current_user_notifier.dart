3235ebcceec9dd7ff70dd52feaefd33b10f0a37d
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-11-30 18:10:10 +0200

    feat(provider): implement a function to update current user data

--- frontend-mobile-flutter/lib/core/providers/current_user_notifier.dart
+++ frontend-mobile-flutter/lib/core/providers/current_user_notifier.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:connect/features/auth/models/user_model.dart';
+import 'package:connect/features/home/models/conversation_model.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'current_user_notifier.g.dart';
@@ -10,5 +13,23 @@ class CurrentUserNotifier extends _$CurrentUserNotifier {
 
   void addUser(UserModel user) {
     state = user;
+  }
+
+  void updateUser({
+    String? username,
+    String? profile_url,
+    String? token,
+    bool? is_online,
+    List<ConversationModel>? conversations,
+  }) {
+    if (state != null) {
+      state = state!.copyWith(
+        username: username,
+        profile_url: profile_url,
+        token: token,
+        is_online: is_online,
+        conversations: conversations,
+      );
+    }
   }
 }
