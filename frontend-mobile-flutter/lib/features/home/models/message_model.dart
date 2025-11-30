1bc2e6836a375f2510c150e41be837eebc0b02e9
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-11-30 06:40:47 +0200

    feat: implement message model

---
+++ frontend-mobile-flutter/lib/features/home/models/message_model.dart
@@ -0,0 +1,107 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first, non_constant_identifier_names
+import 'dart:convert';
+
+class MessageModel {
+  final String id;
+  final String sender;
+  final String receiver;
+  final bool is_text;
+  final String content;
+  final String conversation_id;
+  final bool deleted;
+  final DateTime created_at;
+  MessageModel({
+    required this.id,
+    required this.sender,
+    required this.receiver,
+    required this.is_text,
+    required this.content,
+    required this.conversation_id,
+    required this.deleted,
+    required this.created_at,
+  });
+
+  MessageModel copyWith({
+    String? id,
+    String? sender,
+    String? receiver,
+    bool? is_text,
+    String? content,
+    String? conversation_id,
+    bool? deleted,
+    DateTime? created_at,
+  }) {
+    return MessageModel(
+      id: id ?? this.id,
+      sender: sender ?? this.sender,
+      receiver: receiver ?? this.receiver,
+      is_text: is_text ?? this.is_text,
+      content: content ?? this.content,
+      conversation_id: conversation_id ?? this.conversation_id,
+      deleted: deleted ?? this.deleted,
+      created_at: created_at ?? this.created_at,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'sender': sender,
+      'receiver': receiver,
+      'is_text': is_text,
+      'content': content,
+      'conversation_id': conversation_id,
+      'deleted': deleted,
+      'created_at': created_at,
+    };
+  }
+
+  factory MessageModel.fromMap(Map<String, dynamic> map) {
+    return MessageModel(
+      id: map['id'] ?? "",
+      sender: map['sender'] ?? "",
+      receiver: map['receiver'] ?? "",
+      is_text: map['is_text'] ?? false,
+      content: map['content'] ?? "",
+      conversation_id: map['conversation_id'] ?? "",
+      deleted: map['deleted'] ?? false,
+      created_at: DateTime.parse(map['created_at']),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory MessageModel.fromJson(String source) =>
+      MessageModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'MessageModel(id: $id, sender: $sender, receiver: $receiver, is_text: $is_text, content: $content, conversation_id: $conversation_id, deleted: $deleted, created_at: $created_at)';
+  }
+
+  @override
+  bool operator ==(covariant MessageModel other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id &&
+        other.sender == sender &&
+        other.receiver == receiver &&
+        other.is_text == is_text &&
+        other.content == content &&
+        other.conversation_id == conversation_id &&
+        other.deleted == deleted &&
+        other.created_at == created_at;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        sender.hashCode ^
+        receiver.hashCode ^
+        is_text.hashCode ^
+        content.hashCode ^
+        conversation_id.hashCode ^
+        deleted.hashCode ^
+        created_at.hashCode;
+  }
+}
