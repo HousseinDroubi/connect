30ee65cd3286fe355bac652c3cbf74f5f97c53c0
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-11-30 18:28:45 +0200

    feat: implement 'update data' request from update profile screen

--- frontend-mobile-flutter/lib/features/auth/views/screens/update_profile_screen.dart
+++ frontend-mobile-flutter/lib/features/auth/views/screens/update_profile_screen.dart
@@ -1,13 +1,17 @@
 import 'dart:io';
 
 import 'package:connect/core/providers/current_user_notifier.dart';
+import 'package:connect/core/utils/app_responses.dart';
+import 'package:connect/core/utils/dialog.dart';
 import 'package:connect/core/utils/utils.dart';
+import 'package:connect/features/auth/view_models/auth_view_model.dart';
 import 'package:connect/features/auth/views/widgets/button_widget.dart';
 import 'package:connect/features/auth/views/widgets/profile_widget.dart';
 import 'package:connect/features/auth/views/widgets/text_field_widget.dart';
 import 'package:connect/features/auth/views/widgets/title_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
 
 class UpdateProfileScreen extends ConsumerStatefulWidget {
   const UpdateProfileScreen({super.key});
@@ -33,7 +37,31 @@ class _UpdateProfileScreenState extends ConsumerState<UpdateProfileScreen> {
   final FocusNode newPasswordConfirmationFocusNode = FocusNode();
 
   Future<void> updateProfile() async {
-    // TODO
+    showPopup(popupCase: PopupLoading(context: context));
+    final notifier = ref.read(authViewModelProvider.notifier);
+    final Either<AppFailure, AppSuccess> result = await notifier
+        .updateProfileData(
+          username: usernameController.text,
+          imageFile: imageFile,
+        );
+
+    hidePopup(context);
+
+    final String content;
+
+    switch (result) {
+      case Left(value: AppFailure(message: final message)):
+        content = message;
+        break;
+      case Right(value: AppSuccess(message: final message)):
+        content = message;
+        imageFile = null;
+    }
+
+    showPopup(
+      popupCase: PopupAlert(context: context, popupContent: content),
+    );
+    setState(() {});
   }
 
   Future<void> updatePassword() async {
