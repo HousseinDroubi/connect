f7dd22189706bebe6ccb4e5407ac3fb5443e7087
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-11-30 19:45:19 +0200

    feat: add update profile screen route

--- frontend-mobile-flutter/lib/main.dart
+++ frontend-mobile-flutter/lib/main.dart
@@ -7,6 +7,7 @@ import 'package:connect/features/auth/views/screens/login_screen.dart';
 import 'package:connect/features/auth/views/screens/update_forgotten_password_screen.dart';
 import 'package:connect/features/auth/views/screens/verify_account_screen.dart';
 import 'package:connect/features/home/models/views/screens/home_screen.dart';
+import 'package:connect/features/auth/views/screens/update_profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -67,6 +68,7 @@ class _MyAppState extends ConsumerState<MyApp> {
         "/update_forgotten_password": (context) =>
             UpdateForgottenPasswordScreen(),
         "/home": (context) => HomeScreen(),
+        "/update_profile": (context) => UpdateProfileScreen(),
       },
     );
   }
