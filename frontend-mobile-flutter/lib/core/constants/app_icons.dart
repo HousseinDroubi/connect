1a4fa652f84901ae795e6cc76e48aeea18df80b1
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-12-01 06:01:04 +0200

    chore: add home icons

--- frontend-mobile-flutter/lib/core/constants/app_icons.dart
+++ frontend-mobile-flutter/lib/core/constants/app_icons.dart
@@ -9,4 +9,6 @@ class AppIcons {
   static const String doneIconPath = "assets/icons/done.png";
   static const String errorIconPath = "assets/icons/error.png";
   static const String waitingIconPath = "assets/icons/waiting.png";
+  static const String conversationsIconPath = "assets/icons/conversations.png";
+  static const String searchIconPath = "assets/icons/search.png";
 }
