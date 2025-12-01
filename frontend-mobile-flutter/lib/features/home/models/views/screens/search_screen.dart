1c3bdf2f5298422fd682980d27300ba2e5e9ae94
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-12-01 07:03:18 +0200

    feat: implement search top ui

--- frontend-mobile-flutter/lib/features/home/models/views/screens/search_screen.dart
+++ frontend-mobile-flutter/lib/features/home/models/views/screens/search_screen.dart
@@ -1,3 +1,6 @@
+import 'package:connect/core/classes/person.dart';
+import 'package:connect/core/widgets/title_widget.dart';
+import 'package:connect/features/auth/views/widgets/text_field_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,8 +12,40 @@ class SearchScreen extends ConsumerStatefulWidget {
 }
 
 class _SearchScreenState extends ConsumerState<SearchScreen> {
+  TextEditingController searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
+  Future<List<Person>> searchForUsers() async {
+    throw UnimplementedError();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(18),
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              child: TitleWidget(title: "Search by pin or username"),
+            ),
+            SizedBox(height: 20),
+            TextFieldWidget(
+              isFull: true,
+              title: "Search",
+              hint: "Search by pin or username",
+              nextFunction: searchForUsers,
+              textEditingController: searchController,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
