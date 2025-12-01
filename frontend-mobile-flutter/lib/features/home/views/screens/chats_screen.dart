71c9c3350bb8e5309db06c8ea92c0b146a801fe3
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-12-01 10:56:11 +0200

    feat: implement home screen ui

--- frontend-mobile-flutter/lib/features/home/views/screens/chats_screen.dart
+++ frontend-mobile-flutter/lib/features/home/views/screens/chats_screen.dart
@@ -1,3 +1,7 @@
+import 'package:connect/core/providers/current_user_notifier.dart';
+import 'package:connect/core/widgets/title_widget.dart';
+import 'package:connect/features/home/models/conversation_model.dart';
+import 'package:connect/features/home/views/widgets/user_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,6 +10,39 @@ class ChatsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Placeholder();
+    final List<ConversationModel> conversations = ref.watch(
+      currentUserNotifierProvider.select((user) => user?.conversations ?? []),
+    );
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.all(18),
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              child: TitleWidget(title: "All messages"),
+            ),
+            SizedBox(height: 20),
+            ...conversations.map(
+              (ConversationModel conversation) => Container(
+                margin: EdgeInsets.only(bottom: 15),
+                child: UserWidget(
+                  image_source: conversation.recipient?.profile_url,
+                  is_for_chats: true,
+                  username: conversation.recipient == null
+                      ? "Connected Users"
+                      : conversation.recipient!.username,
+                  chats_last_mesasage: conversation.last_message != null
+                      ? (conversation.last_message!.is_text
+                            ? conversation.last_message!.content
+                            : "Photo")
+                      : null,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
