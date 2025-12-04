2ad9598fb8ede9f430a84115940cd7369062e585
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-12-04 19:29:49 +0200

    refactor: use auth local repository provider notifier instead of current user provider

--- frontend-mobile-flutter/lib/features/home/view_models/home_view_model.dart
+++ frontend-mobile-flutter/lib/features/home/view_models/home_view_model.dart
@@ -3,9 +3,8 @@
 import 'dart:typed_data';
 
 import 'package:connect/core/classes/person.dart';
-import 'package:connect/core/providers/current_user_notifier.dart';
 import 'package:connect/core/utils/app_responses.dart';
-import 'package:connect/features/auth/models/user_model.dart';
+import 'package:connect/features/auth/repositories/auth_local_repository.dart';
 import 'package:connect/features/home/repositories/home_users_repository.dart';
 import 'package:connect/features/home/repositories/messages_repository.dart';
 import 'package:fpdart/fpdart.dart';
@@ -15,27 +14,27 @@ part 'home_view_model.g.dart';
 
 @riverpod
 class HomeViewModel extends _$HomeViewModel {
-  late UserModel? _currentUserNotifier;
   late HomeUsersRepository _homeUsersRepository;
   late MessagesRepository _messagesRepository;
+  late AuthLocalRepository _authLocalRepository;
 
   @override
   void build() {
-    _currentUserNotifier = ref.read(currentUserNotifierProvider);
+    _authLocalRepository = ref.read(authLocalRepositoryProvider);
     _homeUsersRepository = ref.read(homeUsersRepositoryProvider);
     _messagesRepository = ref.read(messagesRepositoryProvider);
   }
 
   Future<Either<AppFailure, List<Person>>> searchUsers(String content) async {
     return await _homeUsersRepository.searchForUsers(
-      token: _currentUserNotifier!.token,
+      token: _authLocalRepository.getToken()!,
       content: content,
     );
   }
 
   Future<Uint8List?> viewImage(String message_id) async {
     return await _messagesRepository.viewImage(
-      token: _currentUserNotifier!.token,
+      token: _authLocalRepository.getToken()!,
       message_id: message_id,
     );
   }
