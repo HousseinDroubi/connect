6ff4b95a492f145c2c9ccb23e669dd929b380e01
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-12-04 16:33:56 +0200

    feat: navigate to conversation screen onTap of user widget

--- frontend-mobile-flutter/lib/features/home/views/widgets/user_widget.dart
+++ frontend-mobile-flutter/lib/features/home/views/widgets/user_widget.dart
@@ -2,6 +2,7 @@
 
 import 'package:connect/core/constants/app_colors.dart';
 import 'package:connect/core/utils/app_nav.dart';
+import 'package:connect/features/home/view_models/conversation_view_model.dart';
 import 'package:connect/features/home/views/widgets/user_image_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -33,73 +34,83 @@ class UserWidget extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return SizedBox(
-      width: double.infinity,
-      height: 60,
-      child: Row(
-        children: [
-          if (is_for_conversation)
-            GestureDetector(
-              onTap: () {
-                AppNav.pop(context);
-              },
-              child: Icon(Icons.keyboard_arrow_left),
-            ),
-          UserImageWidget(image_source: image_source, is_small: false),
-          SizedBox(width: 7),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                username ?? "Connected users",
-                style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
+    return GestureDetector(
+      onTap: pin != null
+          ? () async {
+              await ref
+                  .read(conversationViewModelProvider.notifier)
+                  .getConversationMessages(pin!);
+              AppNav.push(context, "conversation");
+            }
+          : null,
+      child: SizedBox(
+        width: double.infinity,
+        height: 60,
+        child: Row(
+          children: [
+            if (is_for_conversation)
+              GestureDetector(
+                onTap: () {
+                  AppNav.pop(context);
+                },
+                child: Icon(Icons.keyboard_arrow_left),
               ),
-              if (is_for_search && pin != null)
+            UserImageWidget(image_source: image_source, is_small: false),
+            SizedBox(width: 7),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
                 Text(
-                  "pin: #$pin",
-                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+                  username ?? "Connected users",
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
                 ),
-              if (is_for_chats)
-                Text(
-                  chats_last_mesasage == null
-                      ? "No messages yet"
-                      : chats_last_mesasage!,
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: chats_last_mesasage == null
-                        ? FontWeight.w300
-                        : FontWeight.w500,
-                    fontStyle: chats_last_mesasage == null || is_deleted
-                        ? FontStyle.italic
-                        : FontStyle.normal,
+                if (is_for_search && pin != null)
+                  Text(
+                    "pin: #$pin",
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
                   ),
-                ),
-              if (is_for_conversation)
-                is_group
-                    ? Text(
-                        "All connected users",
-                        style: TextStyle(fontStyle: FontStyle.italic),
-                      )
-                    : Row(
-                        children: [
-                          Container(
-                            width: 12,
-                            height: 12,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: is_online
-                                  ? AppColors.onlineColor
-                                  : AppColors.offlineColor,
+                if (is_for_chats)
+                  Text(
+                    chats_last_mesasage == null
+                        ? "No messages yet"
+                        : chats_last_mesasage!,
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: chats_last_mesasage == null
+                          ? FontWeight.w300
+                          : FontWeight.w500,
+                      fontStyle: chats_last_mesasage == null || is_deleted
+                          ? FontStyle.italic
+                          : FontStyle.normal,
+                    ),
+                  ),
+                if (is_for_conversation)
+                  is_group
+                      ? Text(
+                          "All connected users",
+                          style: TextStyle(fontStyle: FontStyle.italic),
+                        )
+                      : Row(
+                          children: [
+                            Container(
+                              width: 12,
+                              height: 12,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: is_online
+                                    ? AppColors.onlineColor
+                                    : AppColors.offlineColor,
+                              ),
                             ),
-                          ),
-                          SizedBox(width: 8),
-                          Text(is_online ? "Online" : "Offline"),
-                        ],
-                      ),
-            ],
-          ),
-        ],
+                            SizedBox(width: 8),
+                            Text(is_online ? "Online" : "Offline"),
+                          ],
+                        ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
