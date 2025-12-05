17359d598c4fcc6fef1218da1660d4588dda3c9a
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-12-05 06:45:50 +0200

    refactor: update function, call delete chat using curret user notifier

--- frontend-mobile-flutter/lib/features/home/view_models/conversation_view_model.dart
+++ frontend-mobile-flutter/lib/features/home/view_models/conversation_view_model.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: non_constant_identifier_names, constant_identifier_names
 
 import 'dart:typed_data';
 
 import 'package:connect/core/providers/current_conversation.dart';
+import 'package:connect/core/providers/current_user_notifier.dart';
 import 'package:connect/core/utils/app_responses.dart';
 import 'package:connect/features/auth/repositories/auth_local_repository.dart';
 import 'package:connect/features/home/models/conversation_model.dart';
@@ -19,6 +20,7 @@ class ConversationViewModel extends _$ConversationViewModel {
   late MessagesRepository _messagesRepository;
   late ConversationRepository _conversationRepository;
   late CurrentConversation _currentConversationNotifier;
+  late CurrentUserNotifier _currentUserNotifier;
 
   @override
   void build() {
@@ -28,6 +30,7 @@ class ConversationViewModel extends _$ConversationViewModel {
     _currentConversationNotifier = ref.read(
       currentConversationProvider.notifier,
     );
+    _currentUserNotifier = ref.read(currentUserNotifierProvider.notifier);
   }
 
   Future<Either<AppFailure, ConversationModel>> getConversationMessages(
@@ -53,12 +56,18 @@ class ConversationViewModel extends _$ConversationViewModel {
     );
   }
 
-  Future<Either<AppFailure, String>> deleteChat(String pin) async {
+  Future<Either<AppFailure, AppSuccess>> deleteChat(String pin) async {
     final result = await _conversationRepository.deleteConversation(
       token: _authLocalRepository.getToken()!,
       pin: pin,
     );
 
-    return result;
+    switch (result) {
+      case Left(value: AppFailure(message: final message)):
+        return Left(AppFailure(message: message));
+      case Right(value: final conversation_id):
+        _currentUserNotifier.deleteChat(conversation_id);
+        return Right(AppSuccess());
+    }
   }
 }
