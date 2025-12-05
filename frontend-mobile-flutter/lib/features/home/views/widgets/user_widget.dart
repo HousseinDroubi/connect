9bd9096c5289b3f0225e791d633fae1b5138f48c
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-12-05 06:05:37 +0200

    refactor(widget): turn widget into ConsumerStatefulWidget

--- frontend-mobile-flutter/lib/features/home/views/widgets/user_widget.dart
+++ frontend-mobile-flutter/lib/features/home/views/widgets/user_widget.dart
@@ -8,7 +8,7 @@ import 'package:connect/features/home/views/widgets/user_image_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class UserWidget extends ConsumerWidget {
+class UserWidget extends ConsumerStatefulWidget {
   final String? username;
   final bool is_for_search;
   final bool is_for_chats;
@@ -19,6 +19,7 @@ class UserWidget extends ConsumerWidget {
   final String? image_source;
   final String? chats_last_mesasage;
   final String? pin;
+
   const UserWidget({
     super.key,
     this.username,
@@ -34,14 +35,19 @@ class UserWidget extends ConsumerWidget {
   });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<UserWidget> createState() => _UserWidgetState();
+}
+
+class _UserWidgetState extends ConsumerState<UserWidget> {
+  @override
+  Widget build(BuildContext context) {
     return InkWell(
-      onTap: pin != null
+      onTap: widget.pin != null
           ? () async {
               showPopup(popupCase: PopupLoading(context: context));
               await ref
                   .read(conversationViewModelProvider.notifier)
-                  .getConversationMessages(pin!);
+                  .getConversationMessages(widget.pin!);
               hidePopup(context);
               AppNav.push(context, "conversation");
             }
@@ -51,45 +57,47 @@ class UserWidget extends ConsumerWidget {
         height: 60,
         child: Row(
           children: [
-            if (is_for_conversation)
+            if (widget.is_for_conversation)
               GestureDetector(
                 onTap: () {
                   AppNav.pop(context);
                 },
                 child: Icon(Icons.keyboard_arrow_left),
               ),
-            UserImageWidget(image_source: image_source, is_small: false),
+            UserImageWidget(image_source: widget.image_source, is_small: false),
             SizedBox(width: 7),
             Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  username ?? "Connected users",
+                  widget.username ?? "Connected users",
                   style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
                 ),
-                if (is_for_search && pin != null)
+                if (widget.is_for_search && widget.pin != null)
                   Text(
-                    "pin: #$pin",
+                    "pin: #$widget.pin",
                     style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
                   ),
-                if (is_for_chats)
+                if (widget.is_for_chats)
                   Text(
-                    chats_last_mesasage == null
+                    widget.chats_last_mesasage == null
                         ? "No messages yet"
-                        : chats_last_mesasage!,
+                        : widget.chats_last_mesasage!,
                     style: TextStyle(
                       fontSize: 15,
-                      fontWeight: chats_last_mesasage == null
+                      fontWeight: widget.chats_last_mesasage == null
                           ? FontWeight.w300
                           : FontWeight.w500,
-                      fontStyle: chats_last_mesasage == null || is_deleted
+                      fontStyle:
+                          widget.chats_last_mesasage == null ||
+                              widget.is_deleted
                           ? FontStyle.italic
                           : FontStyle.normal,
                     ),
                   ),
-                if (is_for_conversation)
-                  is_group
+                if (widget.is_for_conversation)
+                  widget.is_group
                       ? Text(
                           "All connected users",
                           style: TextStyle(fontStyle: FontStyle.italic),
@@ -101,13 +109,13 @@ class UserWidget extends ConsumerWidget {
                               height: 12,
                               decoration: BoxDecoration(
                                 shape: BoxShape.circle,
-                                color: is_online
+                                color: widget.is_online
                                     ? AppColors.onlineColor
                                     : AppColors.offlineColor,
                               ),
                             ),
                             SizedBox(width: 8),
-                            Text(is_online ? "Online" : "Offline"),
+                            Text(widget.is_online ? "Online" : "Offline"),
                           ],
                         ),
               ],
