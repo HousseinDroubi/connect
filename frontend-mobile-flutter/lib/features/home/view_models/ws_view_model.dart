43f55dc2ed54f44b8ecc5bebdef98881ba4e6f27
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-12-06 14:28:40 +0200

    feat: initialize ws view model notifier

---
+++ frontend-mobile-flutter/lib/features/home/view_models/ws_view_model.dart
@@ -0,0 +1,9 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part "ws_view_model.g.dart";
+
+@riverpod
+class WsViewModel extends _$WsViewModel {
+  @override
+  void build() {}
+}
