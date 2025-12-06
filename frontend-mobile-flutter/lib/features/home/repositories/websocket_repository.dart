7e7973bf551bfe09874b51ac87f413e165bbdc52
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-12-06 13:05:42 +0200

    feat: connect ws channel to server and close the channel onDispose

--- frontend-mobile-flutter/lib/features/home/repositories/websocket_repository.dart
+++ frontend-mobile-flutter/lib/features/home/repositories/websocket_repository.dart
@@ -1,9 +1,19 @@
+import 'package:connect/core/constants/server_urls.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 part 'websocket_repository.g.dart';
 
 @Riverpod(keepAlive: true)
 class WebsocketRepository extends _$WebsocketRepository {
+  late WebSocketChannel _channel;
+
   @override
-  void build(Object other) {}
+  void build(Object other) {
+    _channel = WebSocketChannel.connect(Uri.parse(ServerUrls.wsBaseUrl));
+
+    ref.onDispose(() {
+      _channel.sink.close();
+    });
+  }
 }
