7e62907cdfbf0b8e608ca0e4c3d9af224a160973
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-12-06 15:23:51 +0200

    refactor: use currentUserNotifier instead of state

--- frontend-mobile-flutter/lib/features/home/view_models/ws_view_model.dart
+++ frontend-mobile-flutter/lib/features/home/view_models/ws_view_model.dart
@@ -1,5 +1,10 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'dart:convert';
+
 import 'package:connect/core/providers/current_user_notifier.dart';
 import 'package:connect/features/auth/models/user_model.dart';
+import 'package:connect/features/auth/repositories/auth_local_repository.dart';
 import 'package:connect/features/home/repositories/ws_repository.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -7,14 +12,16 @@ part "ws_view_model.g.dart";
 
 @Riverpod(keepAlive: true)
 class WsViewModel extends _$WsViewModel {
-  late UserModel? _currentUserNotifier;
+  late CurrentUserNotifier _currentUserNotifier;
   late WsRepository _wsRepository;
 
   @override
   void build() {
     _wsRepository = ref.read(wsRepositoryProvider);
-    _currentUserNotifier = ref.read(currentUserNotifierProvider);
-    _wsRepository.wsConnect(token: _currentUserNotifier!.token);
+    _currentUserNotifier = ref.read(currentUserNotifierProvider.notifier);
+    _wsRepository.wsConnect(
+      token: ref.read(authLocalRepositoryProvider).getToken()!,
+    );
     wsListenToMessages();
   }
 
