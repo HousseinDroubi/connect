0c4ee0386d89e38d91f5bce9337bc1dce934452a
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-12-06 12:45:59 +0200

    feat: initialize websocket repository

---
+++ frontend-mobile-flutter/lib/features/home/repositories/websocket_repository.dart
@@ -0,0 +1,9 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'websocket_repository.g.dart';
+
+@Riverpod(keepAlive: true)
+class WebsocketRepository extends _$WebsocketRepository {
+  @override
+  void build(Object other) {}
+}
